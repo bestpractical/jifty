1a66034408344d5529f4daa3452ce0365f10b55f
Jesse Vincent <jesse@bestpractical.com>
2005-12-25 01:13:21 +0000

    * First refactoring of Jifty::Script::Schema
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@184 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Script/Schema.pm
+++ lib/Jifty/Script/Schema.pm
@@ -11,48 +11,92 @@ use version;
 use Jifty::DBI::SchemaGenerator;
 
 sub options {
-    return 
-      ("install|i" => "install",
-       "print|p"   => "print",
-       "create|c"  => "create",
-       "force"     => "force",
-       "include|I=s@" => "include",
-       "help|?"    => "help",
-       "man"       => "man");
+    return (
+        "install|i"    => "install",
+        "print|p"      => "print",
+        "create|c"     => "create_database",
+        "force"        => "force",
+        "include|I=s@" => "include",
+        "help|?"       => "help",
+        "man"          => "man"
+    );
 }
 
 sub run {
     my $self = shift;
-    my $docs = \*DATA;
 
-    # Option handling
-    pod2usage( -exitval => 1, -input => $docs ) if  $self->{help};
-    pod2usage( -exitval => 0, -verbose => 2, -input => $docs )
-        if  $self->{man};
+    $self->_check_usage();
+    $self->_setup_environment();
+    $self->_probe_database_existence();
+    $self->create_db() if $self->{create_database};
+    $self->_setup_jifty_stuff();
+    if ( $self->{create_all_tables} ) {
+        $self->create_all_tables();
+    } else {
+        $self->upgrade_tables();
+    }
+}
 
-    pod2usage("$0: Must specify exactly one of --install or --print!")
-        unless $self->{install} xor $self->{print};
+sub _setup_environment {
+    my $self = shift;
+
     # Default to current directory
     push @ARGV, "." unless @ARGV;
 
     # Set up include path
-    my $ProjectRoot = shift || ".";
-    if ( $self->{include} ) {
-        unshift @INC, @{ $self->{include} };
-    }
+    my $ProjectRoot = shift @ARGV || ".";
+    unshift @INC, @{ $self->{include} } if ( $self->{include} );
     unshift @INC, "$ProjectRoot/lib";
 
     # Import Jifty
     Jifty->require                or die $UNIVERSAL::require::ERROR;
     Jifty::Model::Schema->require or die $UNIVERSAL::require::ERROR;
 
-    # We trap the various "die" errors
-   
+}
+
+sub _check_usage {
+    my $self = shift;
+
+    # Option handling
+    my $docs = \*DATA;
+    pod2usage( -exitval => 1, -input => $docs ) if $self->{help};
+    pod2usage( -exitval => 0, -verbose => 2, -input => $docs )
+        if $self->{man};
+    pod2usage("$0: Must specify exactly one of --install or --print!")
+        unless $self->{install} xor $self->{print};
+}
+
+sub _setup_jifty_stuff {
+
+    my $self = shift;
+
+    # Set up application-specific parts
+    $self->{'_application_class'}
+        = Jifty->config->framework('ApplicationClass');
+    $self->{'_schema_generator'}
+        = Jifty::DBI::SchemaGenerator->new( Jifty->handle )
+        or die "Can't make Jifty::DBI::SchemaGenerator";
+
+# This creates a sub "models" which when called, finds packages under
+# $self->{'_application_class'}::Model, requires them, and returns a list of their
+# names.
+    require Module::Pluggable;
+    Module::Pluggable->import(
+        require     => 1,
+        search_path =>
+            [ "Jifty::Model", $self->{'_application_class'} . "::Model" ],
+        sub_name => 'models',
+    );
+}
+
+sub _probe_database_existence {
+    my $self = shift;
+
     my $no_handle = 0;
-    if ($self->{'create'} and $self->{'install'}) { 
+    if ( $self->{'create_database'} and $self->{'install'} ) {
         $no_handle = 1;
     }
-    
+
     eval {
         Jifty->new(
             no_handle        => $no_handle,
@@ -62,239 +106,238 @@ sub run {
 
     if ( $@ =~ /doesn't match application schema version/ ) {
         $self->{upgrade} = 1;
-    }
-    elsif ( $@ =~ /no version in the database/ ) {
-        $self->{tables} = 1;
-    }
-    elsif ( $@ =~ /database .*? does not exist/ ) {
-        $self->{tables} = 1;
-        $self->{create} = 1;
-    }
-    elsif ($@) {
+    } elsif ( $@ =~ /no version in the database/ ) {
+        $self->{create_all_tables} = 1;
+    } elsif ( $@ =~ /database .*? does not exist/ ) {
+        $self->{create_all_tables} = 1;
+        $self->{create_database}   = 1;
+    } elsif ($@) {
         die $@;
-    }
-    elsif ( $self->{create} and $self->{force}) {
-        $self->{tables} = 1;
+    } elsif ( $self->{create_database} and $self->{force} ) {
+        $self->{create_all_tables} = 1;
 
         # Don't fall through to exit
-    }
-    else {
+    } else {
         print "Database is installed and up to date; nothing to do\n";
         exit;
     }
 
-    my $log = Log::Log4perl->get_logger("SchemaTool");
-    $self->create_db() if $self->{create};
+}
 
-    # Set up application-specific parts
-    my $ApplicationClass = Jifty->config->framework('ApplicationClass');
-    my $SG               = Jifty::DBI::SchemaGenerator->new( Jifty->handle )
-        or die "Can't make Jifty::DBI::SchemaGenerator";
+=head2 create_all_tables
+
+Create all tables for this application's models. Generally, this happens on installation.
+
+=cut
+
+sub create_all_tables {
+    my $self = shift;
+
     my $schema = Jifty::Model::Schema->new;
+    my $log    = Log::Log4perl->get_logger("SchemaTool");
+    $log->info(
+        "Generating SQL for application $self->{'_application_class'}...");
 
-    # This creates a sub "models" which when called, finds packages under
-    # $ApplicationClass::Model, requires them, and returns a list of their
-    # names.
-    require Module::Pluggable;
-    Module::Pluggable->import(
-        require     => 1,
-        search_path => [ "Jifty::Model", $ApplicationClass . "::Model" ],
-        sub_name    => 'models',
-    );
+    my $appv
+        = version->new( Jifty->config->framework('Database')->{'Version'} );
 
-    if ( $self->{tables} ) {
-        $log->info("Generating SQL for application $ApplicationClass...");
+    for my $model ( __PACKAGE__->models ) {
 
-        my $appv = version->new(
-            Jifty->config->framework('Database')->{'Version'} );
-
-        for my $model ( __PACKAGE__->models ) {
-
-            # We don't want to get the Collections, or models that have a
-            # 'since' that is after the current application version.
+        # We don't want to get the Collections, or models that have a
+        # 'since' that is after the current application version.
 
        # TODO XXX FIXME:
        #   This *will* try to generate SQL for abstract base classes you might
        #   stick in $AC::Model::.
-                next if not UNIVERSAL::isa( $model, 'Jifty::Record' );
-            do { log->info("Skipping $model"); next }
-                if ( UNIVERSAL::can( $model, 'since' )
-                and $appv < $model->since );
+        next if not UNIVERSAL::isa( $model, 'Jifty::Record' );
+        do { log->info("Skipping $model"); next }
+            if ( UNIVERSAL::can( $model, 'since' )
+            and $appv < $model->since );
 
-            $log->info("Using $model");
-            my $ret = $SG->add_model( $model->new );
-            $ret or die "couldn't add model $model: " . $ret->error_message;
-        }
-
-        if ( $self->{'print'} ) {
-            print $SG->create_table_sql_text;
-        }
-        elsif ( $self->{'install'} ) {
-
-            # Start a transactoin
-            Jifty->handle->begin_transaction;
-
-            # Run all CREATE commands
-            for my $statement ( $SG->create_table_sql_statements ) {
-                my $ret = Jifty->handle->simple_query($statement);
-                $ret or die "error creating a table: " . $ret->error_message;
-            }
-
-            # Update the version in the database
-            $schema->update($appv);
-
-            # Load initial data
-            eval {
-                my $bootstrapper = $ApplicationClass . "::Bootstrap";
-                $bootstrapper->require();
-
-                $bootstrapper->run() if (UNIVERSAL::can($bootstrapper => 'run'));
-            };
-            die $@ if $@;
-
-            # Commit it all
-            Jifty->handle->commit;
-        }
-
+        $log->info("Using $model");
+        my $ret = $self->{'_schema_generator'}->add_model( $model->new );
+        $ret or die "couldn't add model $model: " . $ret->error_message;
     }
-    else {
 
-        # Find current versions
-        my $dbv  = $schema->in_db;
-        my $appv = version->new(
-            Jifty->config->framework('Database')->{'Version'} );
-        if ( $appv < $dbv ) {
-            print "Version $appv from module older than $dbv in database!\n";
-            exit;
+    if ( $self->{'print'} ) {
+        print $self->{'_schema_generator'}->create_table_sql_text;
+    } elsif ( $self->{'install'} ) {
+
+        # Start a transactoin
+        Jifty->handle->begin_transaction;
+
+        # Run all CREATE commands
+        for my $statement (
+            $self->{'_schema_generator'}->create_table_sql_statements )
+        {
+            my $ret = Jifty->handle->simple_query($statement);
+            $ret or die "error creating a table: " . $ret->error_message;
         }
-        elsif ( $appv == $dbv ) {
 
-            # Shouldn't happen
-            print "Version $appv up to date.\n";
-            exit;
-        }
-        $log->info(
-            "Gerating SQL to update $ApplicationClass $dbv database to $appv"
-        );
+        # Update the version in the database
+        $schema->update($appv);
 
-        my %UPGRADES;
-
-        # Figure out what versions the upgrade knows about.
+        # Load initial data
         eval {
-            my $upgrader = $ApplicationClass . "::Upgrade";
-            $upgrader->require();
-            $UPGRADES{$_} = [ $upgrader->upgrade_to($_) ]
-                for
-                grep { $appv >= version->new($_) and $dbv < version->new($_) }
-                $upgrader->versions();
+            my $bootstrapper = $self->{'_application_class'} . "::Bootstrap";
+            $bootstrapper->require();
+
+            $bootstrapper->run()
+                if ( UNIVERSAL::can( $bootstrapper => 'run' ) );
         };
+        die $@ if $@;
 
-        for my $model ( __PACKAGE__->models ) {
+        # Commit it all
+        Jifty->handle->commit;
+    }
+}
 
-            # We don't want to get the Collections, for example.
-            do { next }
-                unless UNIVERSAL::isa( $model, 'Jifty::Record' );
+=head2 upgrade_tables 
 
-            # Set us up the table
-            $model = $model->new;
-            my $t = $SG->_db_schema_table_from_model($model);
+Upgrade your app's tables to match your current model.
 
-            # If this whole table is new
-            if (    UNIVERSAL::can( $model, "since" )
-                and $appv >= $model->since
-                and $dbv < $model->since )
-            {
+=cut
 
-                # Create it
-                unshift @{ $UPGRADES{ $model->since } },
-                    $t->sql_create_table( Jifty->handle->dbh );
-            }
-            else {
+sub upgrade_tables {
+    my $self = shift;
 
-                # Go through the columns
-                for my $column ( $model->columns ) {
-                    next
-                        if defined $column->refers_to
-                        and defined $column->by;
+    my $schema = Jifty::Model::Schema->new;
+    my $log    = Log::Log4perl->get_logger("SchemaTool");
 
-                    # If they're old, drop them
-                    if (    defined $column->until
-                        and $appv >= $column->until
-                        and $dbv < $column->until )
-                    {
-                        push @{ $UPGRADES{ $column->until } },
-                            "ALTER TABLE "
-                            . $model->table
-                            . " DROP COLUMN "
-                            . $column->name;
-                    }
+    # Find current versions
+    my $dbv = $schema->in_db;
+    my $appv
+        = version->new( Jifty->config->framework('Database')->{'Version'} );
+    if ( $appv < $dbv ) {
+        print "Version $appv from module older than $dbv in database!\n";
+        exit;
+    } elsif ( $appv == $dbv ) {
 
-                    # If they're new, add them
-                    if (    defined $column->since
-                        and $appv >= $column->since
-                        and $dbv < $column->since )
-                    {
-                        unshift @{ $UPGRADES{ $column->since } },
-                            "ALTER TABLE "
-                            . $model->table
-                            . " ADD COLUMN "
-                            . $t->column( $column->name )
-                            ->line( Jifty->handle->dbh );
-                    }
+        # Shouldn't happen
+        print "Version $appv up to date.\n";
+        exit;
+    }
+    $log->info(
+        "Gerating SQL to update $self->{'_application_class'} $dbv database to $appv"
+    );
+
+    my %UPGRADES;
+
+    # Figure out what versions the upgrade knows about.
+    eval {
+        my $upgrader = $self->{'_application_class'} . "::Upgrade";
+        $upgrader->require();
+        $UPGRADES{$_} = [ $upgrader->upgrade_to($_) ]
+            for grep { $appv >= version->new($_) and $dbv < version->new($_) }
+            $upgrader->versions();
+    };
+
+    for my $model ( __PACKAGE__->models ) {
+
+        # We don't want to get the Collections, for example.
+        do {next}
+            unless UNIVERSAL::isa( $model, 'Jifty::Record' );
+
+        # Set us up the table
+        $model = $model->new;
+        my $t = $self->{'_schema_generator'}
+            ->_db_schema_table_from_model($model);
+
+        # If this whole table is new
+        if (    UNIVERSAL::can( $model, "since" )
+            and $appv >= $model->since
+            and $dbv < $model->since )
+        {
+
+            # Create it
+            unshift @{ $UPGRADES{ $model->since } },
+                $t->sql_create_table( Jifty->handle->dbh );
+        } else {
+
+            # Go through the columns
+            for my $column ( $model->columns ) {
+                next
+                    if defined $column->refers_to
+                    and defined $column->by;
+
+                # If they're old, drop them
+                if (    defined $column->until
+                    and $appv >= $column->until
+                    and $dbv < $column->until )
+                {
+                    push @{ $UPGRADES{ $column->until } },
+                        "ALTER TABLE "
+                        . $model->table
+                        . " DROP COLUMN "
+                        . $column->name;
+                }
+
+                # If they're new, add them
+                if (    defined $column->since
+                    and $appv >= $column->since
+                    and $dbv < $column->since )
+                {
+                    unshift @{ $UPGRADES{ $column->since } },
+                        "ALTER TABLE "
+                        . $model->table
+                        . " ADD COLUMN "
+                        . $t->column( $column->name )
+                        ->line( Jifty->handle->dbh );
                 }
             }
         }
+    }
 
-        if ( $self->{'print'} ) {
-            for ( map { @{ $UPGRADES{$_} } }
-                sort { version->new($a) <=> version->new($b) }
-                keys %UPGRADES )
-            {
-                if ( ref $_ ) {
-                    print "-- Upgrade subroutine:\n";
-                    require Data::Dumper;
-                    $Data::Dumper::Pad     = "-- ";
-                    $Data::Dumper::Deparse = 1;
-                    $Data::Dumper::Indent  = 1;
-                    $Data::Dumper::Terse   = 1;
-                    print Data::Dumper::Dumper($_);
-                }
-                else {
-                    print "$_;\n";
+    if ( $self->{'print'} ) {
+        for (
+            map  { @{ $UPGRADES{$_} } }
+            sort { version->new($a) <=> version->new($b) }
+            keys %UPGRADES
+            )
+        {
+            if ( ref $_ ) {
+                print "-- Upgrade subroutine:\n";
+                require Data::Dumper;
+                $Data::Dumper::Pad     = "-- ";
+                $Data::Dumper::Deparse = 1;
+                $Data::Dumper::Indent  = 1;
+                $Data::Dumper::Terse   = 1;
+                print Data::Dumper::Dumper($_);
+            } else {
+                print "$_;\n";
+            }
+        }
+    } elsif ( $self->{'install'} ) {
+        Jifty->handle->begin_transaction;
+        for my $version (
+            sort { version->new($a) <=> version->new($b) }
+            keys %UPGRADES
+            )
+        {
+            $log->info("Upgrading through $version");
+            for my $thing ( @{ $UPGRADES{$version} } ) {
+                if ( ref $thing ) {
+                    $log->info("Running upgrade script");
+                    $thing->();
+                } else {
+                    my $ret = Jifty->handle->simple_query($thing);
+                    $ret
+                        or die "error updating a table: "
+                        . $ret->error_message;
                 }
             }
         }
-        elsif ( $self->{'install'} ) {
-            Jifty->handle->begin_transaction;
-            for my $version ( sort { version->new($a) <=> version->new($b) }
-                keys %UPGRADES )
-            {
-                $log->info("Upgrading through $version");
-                for my $thing ( @{ $UPGRADES{$version} } ) {
-                    if ( ref $thing ) {
-                        $log->info("Running upgrade script");
-                        $thing->();
-                    }
-                    else {
-                        my $ret = Jifty->handle->simple_query($thing);
-                        $ret
-                            or die "error updating a table: "
-                            . $ret->error_message;
-                    }
-                }
-            }
-            $schema->update($appv);
-            $log->info("Upgraded to version $appv");
-            Jifty->handle->commit;
-        }
+        $schema->update($appv);
+        $log->info("Upgraded to version $appv");
+        Jifty->handle->commit;
     }
 }
 
 sub create_db {
-    my $self = shift;
+    my $self     = shift;
     my $handle   = Jifty::Handle->new();
     my $database = lc Jifty->config->framework('Database')->{'Database'};
-    my $driver = Jifty->config->framework('Database')->{'Driver'};
+    my $driver   = Jifty->config->framework('Database')->{'Driver'};
 
     if ( $self->{'print'} ) {
         print "DROP DATABASE $database;\n" if $self->{'force'};
@@ -304,21 +347,22 @@ sub create_db {
 
     # Everything but the template1 database is assumed
     my %connect_args;
-    $connect_args{'database'} = 'template1' if ($driver eq 'Pg');
+    $connect_args{'database'} = 'template1' if ( $driver eq 'Pg' );
     $handle->connect(%connect_args);
 
-    if ($driver eq 'SQLite') { 
+    if ( $driver eq 'SQLite' ) {
         unlink($database);
     } else {
-         $handle->simple_query("DROP DATABASE $database");
+        $handle->simple_query("DROP DATABASE $database");
 
     }
-    if ($driver ne 'SQLite') { 
+    if ( $driver ne 'SQLite' ) {
         $handle->simple_query("CREATE DATABASE $database");
     }
     $handle->disconnect;
+
     # reinit our handle
-    Jifty->handle(Jifty::Handle->new());
+    Jifty->handle( Jifty::Handle->new() );
     Jifty->handle->connect();
 }
 
