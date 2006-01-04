f9f4966985fc17b36f8f8dcd975c6c1847e19d5a
Jesse Vincent <jesse@bestpractical.com>
2006-01-04 18:34:29 +0000

    * Jifty::Everything shouldn't import anything
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@451 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Everything.pm
+++ lib/Jifty/Everything.pm
@@ -9,58 +9,56 @@ Jifty::Everything - Load all of the important Jifty modules at once.
 
 =cut
 
-# Could use Module::Pluggable, I guess.
+use Jifty ();
+use Jifty::Dispatcher ();
+use Jifty::Object ();
+use Jifty::Config ();
+use Jifty::Handle ();
+use Jifty::ClassLoader ();
+use Jifty::Util ();
 
-use Jifty;
-use Jifty::Dispatcher;
-use Jifty::Object;
-use Jifty::Config;
-use Jifty::Handle;
-use Jifty::ClassLoader;
-use Jifty::Util;
-
-use Jifty::Record;
-use Jifty::Collection;
-use Jifty::Action;
-use Jifty::Action::Record;
-use Jifty::Action::Record::Create;
-use Jifty::Action::Record::Update;
-use Jifty::Action::Record::Delete;
+use Jifty::Record ();
+use Jifty::Collection ();
+use Jifty::Action ();
+use Jifty::Action::Record ();
+use Jifty::Action::Record::Create ();
+use Jifty::Action::Record::Update ();
+use Jifty::Action::Record::Delete ();
 
 
-use Jifty::Continuation;
+use Jifty::Continuation ();
 
-use Jifty::LetMe;
+use Jifty::LetMe ();
 
-use Jifty::Logger;
-use Jifty::Handler;
+use Jifty::Logger ();
+use Jifty::Handler ();
 
-use Jifty::MasonInterp;
+use Jifty::MasonInterp ();
 
-use Jifty::Model::Schema;
+use Jifty::Model::Schema ();
 
 
 
-use Jifty::Request;
-use Jifty::Request::Mapper;
-use Jifty::Result;
-use Jifty::Response;
-use Jifty::CurrentUser;
+use Jifty::Request ();
+use Jifty::Request::Mapper ();
+use Jifty::Result ();
+use Jifty::Response ();
+use Jifty::CurrentUser ();
 
 # We can _not_ load Server.pm unless we're in a Server context because
 # HTTP::Server::Simple::Mason bastardizes HTML::Mason::FakeApache::send_http_header
 # with hook::lexwrap
 #use Jifty::Server;
 
-use Jifty::Web;
-use Jifty::Web::Session;
-use Jifty::Web::PageRegion;
-use Jifty::Web::Form;
-use Jifty::Web::Form::Clickable;
-use Jifty::Web::Form::Element;
-use Jifty::Web::Form::Link;
-use Jifty::Web::Form::Field;
-use Jifty::Web::Menu;
+use Jifty::Web ();
+use Jifty::Web::Session ();
+use Jifty::Web::PageRegion ();
+use Jifty::Web::Form ();
+use Jifty::Web::Form::Clickable ();
+use Jifty::Web::Form::Element ();
+use Jifty::Web::Form::Link ();
+use Jifty::Web::Form::Field ();
+use Jifty::Web::Menu ();
 
 use Module::Pluggable;
 Module::Pluggable->import(search_path => ['Jifty::Web::Form::Field'], require => 1);
