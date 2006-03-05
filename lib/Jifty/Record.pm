0b770ef108adb948495a69334e32560330689e08
Jesse Vincent <jesse@bestpractical.com>
2006-03-05 08:41:02 +0000

    * Switch back to record::cachable for now. Memcached is busted still
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@662 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Record.pm
+++ lib/Jifty/Record.pm
@@ -15,7 +15,7 @@ representation; that is, it is also a L<Jifty::DBI::Record> as well.
 =cut
 
 use base qw/Jifty::Object/;
-use base qw/Jifty::DBI::Record::Memcached/;
+use base qw/Jifty::DBI::Record::Cachable/;
 
 sub _init {
     my $self = shift;
