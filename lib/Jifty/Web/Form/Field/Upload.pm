8bd779c9fbfaf636b046a5820f11ed4857e2c9ac
Alex Vandiver <alexmv@bestpractical.com>
2006-03-14 21:06:38 +0000

    * Additional bit of docs to clear up upload field confusion
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@689 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Web/Form/Field/Upload.pm
+++ lib/Jifty/Web/Form/Field/Upload.pm
@@ -3,6 +3,19 @@ use strict;
  
 package Jifty::Web::Form::Field::Upload;
 
+=head1 NAME
+
+Jifty::Web::Form::Field::Upload - File upload field
+
+=head1 DESCRIPTION
+
+An input field that renders using C<< <input type="file" /> >>.  The
+argument value that the action recieves from this field via
+L<Jifty::Action/argument_value> will be a filehandle, which can be
+read in the usual ways.
+
+=cut
+
 use base qw/Jifty::Web::Form::Field/;
 
 =head2 render_widget
