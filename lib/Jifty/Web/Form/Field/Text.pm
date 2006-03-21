b5246444e686890017d6dd510fdc23785848a2f2
Thomas Sibley <trs@bestpractical.com>
2006-03-21 03:24:57 +0000

    Add the class 'text' to text fields
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@715 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Web/Form/Field/Text.pm
+++ lib/Jifty/Web/Form/Field/Text.pm
@@ -12,6 +12,9 @@ Jifty::Web::Form::Field::Text - Renders as a small text field
 
 our $VERSION = 1;
 
-
+sub classes {
+    my $self = shift;
+    return join(' ', 'text', ($self->SUPER::classes));
+}
 
 1;
