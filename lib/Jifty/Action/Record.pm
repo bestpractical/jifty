6d74a93dc9e62b74ffa16c1bcc9b9bebdafd66cb
Audrey Tang <audreyt@audreyt.org>
2006-04-13 00:58:31 +0000

    * Jifty::Action::Record: The comment claims:         # If we're hand-coding a render_as, hints or label, let's use it.   but then it ignored hand-coded values in the code.  One of them is   lying, and I agree with the comment, so the code goes away. ;)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@844 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Action/Record.pm
+++ lib/Jifty/Action/Record.pm
@@ -215,7 +215,7 @@ sub arguments {
         # If we're hand-coding a render_as, hints or label, let's use it.
         for ( qw(render_as label hints length mandatory sort_order)) { 
         
-            if ( defined $column->$_ and not $info->{$_}) {
+            if ( defined $column->$_ ) {
                  $info->{$_} = $column->$_;
             }
         }
