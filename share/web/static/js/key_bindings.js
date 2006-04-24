5fc770d00dfa14df72eccfc2e5d3e37134868787
Thomas Sibley <trs@bestpractical.com>
2006-04-24 19:36:37 +0000

    Turn the key bindings into a Jifty library and fix propagation problems in Firefox
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/jifty-jsan@916 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- share/web/static/js/key_bindings.js
+++ share/web/static/js/key_bindings.js
@@ -1,69 +1,85 @@
-// Copyright 2004-2005, Best Practical Solutions, LLC
+// Copyright 2004-2006, Best Practical Solutions, LLC
 // This Library is licensed to you under the same terms as Perl 5.x
 
-var bindings = Array;
+JSAN.use("DOM.Events");
 
-document.onkeydown = doClick;
-function doClick(e) {
-    var targ;
-        if (!e) var e = window.event;
-            if (e.target) targ = e.target;
-            else if (e.srcElement) targ = e.srcElement;
-        if (targ.nodeType == 3) // defeat Safari bug
-                targ = targ.parentNode;
-   
-   // safari or mozilla
-   if ( ( ! e.metaKey && ! e.altKey &&  ! e.ctrlKey )
-        && (
-        (targ == document.body) || 
-       (targ ==  document.getElementsByTagName('html')[0])  
-        ) ){
-	var code = String.fromCharCode(e.keyCode);
-    var binding = getKeyBinding(code);
-   if (binding) {
-   if (binding["action"] == "goto") {
-        document.location = (binding["data"]);
-    } 
-   else if (binding["action"] == "focus") {
-      var elements = document.getElementsByName(binding["data"]);
-       elements[0].focus();
+if ( typeof Jifty == "undefined" ) Jifty = {};
+
+Jifty.KeyBindings = {
+    bindings: new Array(),
+    listener: null,
+
+    activate: function() {
+        Jifty.KeyBindings.listener = DOM.Events.addListener(
+                                        window,
+                                        "keydown",
+                                        Jifty.KeyBindings.doClick
+                                     );
+    },
+
+    deactivate: function() {
+        DOM.Events.removeListener(Jifty.KeyBindings.listener);
+    },
+
+    doClick: function(e) {
+        if (e.target.nodeType == 3) // defeat Safari bug
+            e.target = e.target.parentNode;
+       
+        // Safari or Mozilla
+        if (    ( !e.metaKey && !e.altKey && !e.ctrlKey )
+             && (    (e.target == document.body)
+                  || (e.target == document.getElementsByTagName("html")[0]) ))
+        {
+            var code    = String.fromCharCode(e.keyCode);
+            var binding = Jifty.KeyBindings.get(code);
+            
+            if (binding) {
+                e.preventDefault();
+                
+                if (binding["action"] == "goto") {
+                    document.location = (binding["data"]);
+                }
+                else if (binding["action"] == "focus") {
+                    var elements = document.getElementsByName(binding["data"]);
+                    elements[0].focus();
+                }
+                else if (binding["action"] == "click") {
+                    var elements = document.getElementsByName(binding["data"]);
+                    elements[0].click();
+                }
+            }
+        }
+    },
+
+    add: function(key, action, data, label) {
+        var binding = new Array();
+        binding["action"]  = action;
+        binding["data"]    = data;
+        binding["label"]   = label;
+        Jifty.KeyBindings.bindings[key] = binding;
+    },
+
+    get: function(key) {
+        return Jifty.KeyBindings.bindings[key];
+    },
+
+    writeLegend: function() {
+        var content = "";
+        
+        for (var key in Jifty.KeyBindings.bindings) {
+            if ( Jifty.KeyBindings.bindings[key]["label"] ) {
+                content = content + '<div class="keybinding"><dt>'+key + '</dt>' +'<dd>'+Jifty.KeyBindings.bindings[key]['label'] +'</dd></div>'; 
+            }
+        }
+        
+        if (content) {
+            document.write('<div class="keybindings">');
+            document.write('<dl class="keybindings">');
+            document.write(content);
+            document.write('</dl>');
+            document.write('</div>');
+        }
     }
-   else if (binding["action"] == "click") {
-      var elements = document.getElementsByName(binding["data"]);
-       elements[0].click();
-    }
-
- }     
-
-}
-}
-
-function addKeyBinding(key, action, data, label) {
-    var binding = new Array;
-    binding["action"] = action;
-    binding["data"] = data;
-    binding["label"] = label;
-    bindings[key] = binding;
 }
 
 
-function getKeyBinding(key) {
-    return(bindings[key]);
-}
-
-
-function writeKeyBindingLegend() {
-    var content = '';
-    for  (var key in bindings) {
-    if ( bindings[key]['label']) {
-    content = content + '<div class="keybinding"><dt>'+key + '</dt>' +'<dd>'+bindings[key]['label'] +'</dd></div>'; 
-    }
-    }
-    if (content) {
-    document.write('<div class="keybindings">');
-    document.write('<dl class="keybindings">');
-    document.write(content);
-    document.write('</dl>');
-    document.write('</div>');
-    }
-}
