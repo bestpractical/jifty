06bbcd475c5c7099c46a367af7f47f8520439519
Nelson Elhage <nelhage@bestpractical.com>
2006-06-14 15:06:35 +0000

    (empty commit message)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@1281 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- t/Continuations/lib/Continuations/Dispatcher.pm
+++ t/Continuations/lib/Continuations/Dispatcher.pm
@@ -4,10 +4,12 @@ use Jifty::Dispatcher -base;
 
 before '/help' => run {
     Jifty->web->tangent(url => '/index-help.html');
-};
+}
 
 before '/index-help.html' => run {
     set 'getting_help' => 1;
-};
+}
+
+on '*' => show;
 
 1;
