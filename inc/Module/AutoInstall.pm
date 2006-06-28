56d6108d8c081c0782fd627bb5f8aacf9049760c
Alex Vandiver <alexmv@bestpractical.com>
2006-06-28 19:47:56 +0000

    * Bumped Module::Install to 0.630
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@1392 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- inc/Module/AutoInstall.pm
+++ inc/Module/AutoInstall.pm
@@ -639,7 +639,9 @@ sub _version_check {
 
     # check for version numbers that are not in decimal format
     if ( ref($cur) or ref($min) or $cur =~ /v|\..*\./ or $min =~ /v|\..*\./ ) {
-        if ( $version::VERSION or defined( _load('version') ) ) {
+        if ( ( $version::VERSION or defined( _load('version') )) and
+             version->can('new') 
+            ) {
 
             # use version.pm if it is installed.
             return (
@@ -750,4 +752,4 @@ installdeps ::
 
 __END__
 
-#line 988
+#line 990
