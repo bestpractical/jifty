6bc0601171e30c3ad7e5c202b355c51f36012b95
Thomas Sibley <trs@bestpractical.com>
2006-06-28 23:21:46 +0000

    Improved findPosX/Y
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@1396 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- share/web/static/js/jifty_utils.js
+++ share/web/static/js/jifty_utils.js
@@ -2,35 +2,37 @@
 if (typeof Jifty == "undefined") Jifty = { };
 
 Jifty.Utils = {
-    /* From http://www.quirksmode.org/js/findpos.html */
-    findPosX: function(obj) {
-        var curleft = 0;
-        if (obj.offsetParent)
-        {
-            while (obj.offsetParent)
-            {
-                curleft += obj.offsetLeft
-                obj = obj.offsetParent;
-            }
-        }
-        else if (obj.x)
-            curleft += obj.x;
-        return curleft;
+    /* From http://blog.firetree.net/2005/07/04/javascript-find-position/ */
+    findPosX: function(obj)
+    {
+      var curleft = 0;
+      if(obj.offsetParent)
+          while(1) 
+          {
+            curleft += obj.offsetLeft;
+            if(!obj.offsetParent)
+              break;
+            obj = obj.offsetParent;
+          }
+      else if(obj.x)
+          curleft += obj.x;
+      return curleft;
     },
 
-    findPosY: function(obj) {
-        var curtop = 0;
-        if (obj.offsetParent)
-        {
-            while (obj.offsetParent)
-            {
-                curtop += obj.offsetTop
-                obj = obj.offsetParent;
-            }
-        }
-        else if (obj.y)
-            curtop += obj.y;
-        return curtop;
+    findPosY: function(obj)
+    {
+      var curtop = 0;
+      if(obj.offsetParent)
+          while(1)
+          {
+            curtop += obj.offsetTop;
+            if(!obj.offsetParent)
+              break;
+            obj = obj.offsetParent;
+          }
+      else if(obj.y)
+          curtop += obj.y;
+      return curtop;
     },
 
     isMSIE: false
