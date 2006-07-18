464f84466350a4fe00c425cd2f66ca9da4e68a4a
Audrey Tang <audreyt@audreyt.org>
2006-07-18 01:04:45 +0000

    * Declarative schema landed.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/moose@1603 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

---
+++ lib/Jifty/Param/Schema.pm
@@ -0,0 +1,28 @@
+package Jifty::Param::Schema;
+
+use Jifty::I18N;
+use Jifty::Param;
+use Scalar::Defer;
+use Object::Declare ['Jifty::Param'];
+use Exporter::Lite;
+use Class::Data::Inheritable;
+
+our @EXPORT = qw( defer lazy param schema from );
+
+sub schema (&) {
+    my $code = shift;
+    my $from = caller;
+    local *_ = sub {
+        my $args = \@_;
+        defer { local *_; Jifty::I18N->new; _(@$args) };
+    };
+
+    Class::Data::Inheritable::mk_classdata($from => qw/PARAMS/);
+    $from->PARAMS( &declare($code) );
+
+    no strict 'refs';
+    push @{$from . '::ISA'}, 'Jifty::Action';
+    return;
+}
+
+1;
