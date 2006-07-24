f5e731cda344f0163a7b761e630e666cac9420e1
Audrey Tang <audreyt@audreyt.org>
2006-07-24 22:52:21 +0000

    * Jifty::ClassLoader - Make Jifty::Handle a CL'ed module as well,   so MyApp::Handle can implement scary magick of its own.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@1662 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty.pm
+++ lib/Jifty.pm
@@ -262,7 +262,10 @@ sub setup_database_connection {
         or __PACKAGE__->config->framework('SkipDatabase')
         or not __PACKAGE__->config->framework('Database') )
     {
-        __PACKAGE__->handle( Jifty::Handle->new() );
+
+        my $handle_class = (Jifty->config->framework('ApplicationClass') . "::Handle");
+        Jifty::Util->require( $handle_class );
+        __PACKAGE__->handle( $handle_class->new );
         __PACKAGE__->handle->connect();
         __PACKAGE__->handle->check_schema_version();
     }
