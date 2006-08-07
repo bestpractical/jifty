b57e55cd81194536b4558a26744cd7a02ab141de
Nelson Elhage <nelhage@bestpractical.com>
2006-08-07 16:57:08 +0000

    Fixing tests when using JDBI::Record::Memcached and setting things in the database from test scripts
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@1775 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Test.pm
+++ lib/Jifty/Test.pm
@@ -42,16 +42,24 @@ sub setup {
     $ENV{'JIFTY_TEST_CONFIG'} ||= $test_config;
     Jifty::Test->builder->{test_config} = $test_config;
     {
-        # Cache::Memcached stores things. And doesn't let them
-        # expire from the cache easily. This is fine in production,
-        # but during testing each test script needs its own namespace.
-        # we use the pid.
+        # Cache::Memcached stores things. And doesn't let them expire
+        # from the cache easily. This is fine in production, but
+        # during testing each test script needs its own namespace.  we
+        # use the pid of the current process, and save it so the keys
+        # stays the same when we fork
+      {
+          package Jifty::Record;
+          no warnings qw/redefine/;
 
-        no warnings qw/redefine/;
-    
-        sub Jifty::Record::cache_key_prefix {
-            'jifty-test-' . $$;
-        }
+          use vars qw/$cache_key_prefix/;
+
+          $cache_key_prefix = "jifty-test-$$";
+        
+          sub cache_key_prefix {
+              $Jifty::Record::cache_key_prefix;
+          }
+      }
+        
     }
     my $root = Jifty::Util->app_root;
 
