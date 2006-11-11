596ab39bb4f4d855c57c3520e198dd38c7fb6dd6
Yves Agostini <agostini@univ-metz.fr>
2006-11-11 13:32:21 +0000

    Update MANIFEST, beginning of POD
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@2132 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- plugins/Login/lib/Jifty/Plugin/Login.pm
+++ plugins/Login/lib/Jifty/Plugin/Login.pm
@@ -1,8 +1,37 @@
 use strict;
 use warnings;
 
+=head1 NAME
+
+Jifty::Plugin::Login
+
+=cut
+
 package Jifty::Plugin::Login;
 use base qw/Jifty::Plugin/;
+
+=head1 SYNOPSIS
+
+ in etc/config.yml
+    Plugins:
+      - Login: {}
+
+ in your application Model/User.pm
+   use strict;
+   package YourApp::Model::User;
+   use base qw/Jifty::Plugin::Login::Model::User/;
+   1;
+
+ in your application, you can use
+  http://localhost:8888/login
+                       /logout
+                       /signup
+                       /chgpasswd
+                       /passwordreminder
+
+=head1 DESCRIPTION
+
+=cut
 
 # Your plugin goes here.  If takes any configuration or arguments, you
 # probably want to override L<Jifty::Plugin/init>.
