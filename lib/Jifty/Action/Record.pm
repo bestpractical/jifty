b654565cd9d45efcf78c5e3f6d0627cf55101365
Audrey Tang <audreyt@audreyt.org>
2006-12-03 09:49:06 +0000

    * Jifty::Action::Record - Autocompletion now lists only the parts   that matches the user-input as prefix.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@2292 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Action/Record.pm
+++ lib/Jifty/Action/Record.pm
@@ -274,9 +274,9 @@ sub arguments {
 
                     $collection->unlimit;
                     $collection->rows_per_page(20);
-                    $collection->limit(column => $field, value => $value, operator => 'STARTSWITH') if length($value);
-                    $collection->limit(column => $field, value => 'NULL', operator => 'IS NOT');
-                    $collection->limit(column => $field, value => '', operator => '!=');
+                    $collection->limit(column => $field, value => $value, operator => 'STARTSWITH', entry_aggregator => 'AND') if length($value);
+                    $collection->limit(column => $field, value => 'NULL', operator => 'IS NOT', entry_aggregator => 'AND');
+                    $collection->limit(column => $field, value => '', operator => '!=', entry_aggregator => 'AND');
                     $collection->columns('id', $field);
                     $collection->order_by(column => $field);
                     $collection->group_by(column => $field);
