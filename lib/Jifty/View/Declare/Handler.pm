a428fd8346f4483e6fe3baad19f125161e440e03
Jesse Vincent <jesse@bestpractical.com>
2006-12-22 00:21:47 +0000

    * Add support for sending the http header with Template::Declare
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/template-declare@2418 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/View/Declare/Handler.pm
+++ lib/Jifty/View/Declare/Handler.pm
@@ -24,23 +24,36 @@ sub new {
 
 
 sub show {
-    my $self = shift;
+    my $self          = shift;
     my $code_template = shift;
 
     no warnings qw/redefine utf8/;
     local *Jifty::Web::out = sub {
-        shift;  # Remove the $self in Jifty::Web->out
+        shift;    # Turn the method into a function
+        my $r = Jifty->handler->apache;
+        unless ( $r->http_header_sent ) {
+            $r->content_type || $r->content_type('text/html; charset=utf-8');    # Set up a default
+            if ( $r->content_type =~ /charset=([\w-]+)$/ ) {
+                my $enc = $1;
+                binmode *STDOUT, ((lc($enc) =~ /utf-?8/)  ? ":utf8" : "encoding($enc)");
+            }
+            eval {
+               #HTML::Mason::FakeApache's send_http_header uses "print STDOUT"
+               # this reimplements that.
+               Template::Declare::outs( $r->http_header );
+               $r->{http_header_sent} = 1;
+            };
+        }
+
         goto &Template::Declare::Tags::outs;
     };
 
     local $Template::Declare::Tags::BUFFER = '';
-
     my $rv = Template::Declare::Tags::show($code_template);
 
     # XXX - Kluge - Before $r->send_http_headers is fixed for real, escape all non-latin1 characters.
-    print STDOUT Encode::encode(latin1 => $rv, &Encode::FB_XMLCREF)
-        unless Jifty->handler->apache->http_header_sent;
-
+    print STDOUT Encode::encode( latin1 => $rv, &Encode::FB_XMLCREF )
+    unless Jifty->handler->apache->http_header_sent;
     return undef;
 }
 
