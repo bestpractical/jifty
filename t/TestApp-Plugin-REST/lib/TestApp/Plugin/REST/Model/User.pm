31262124fcd4456bc416679ee68e5695fd59f9c5
Jesse Vincent <jesse@bestpractical.com>
2007-01-26 14:25:56 +0000

    * Modernization of model declarations for compatibility with new Object::Declare based Jifty::DBI
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@2602 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- t/TestApp-Plugin-REST/lib/TestApp/Plugin/REST/Model/User.pm
+++ t/TestApp-Plugin-REST/lib/TestApp/Plugin/REST/Model/User.pm
@@ -1,9 +1,12 @@
-package TestApp::Plugin::REST::Model::User::Schema;
+package TestApp::Plugin::REST::Model::User;
+use base qw/TestApp::Plugin::REST::Record/;
+
 use Jifty::DBI::Schema;
 
 # Your column definitions go here.  See L<Jifty::DBI::Schema> for
 # documentation about how to write column definitions.
 
+use Jifty::Record schema {
 column 'name' =>
   type is 'text',
   is mandatory;
@@ -13,10 +16,8 @@ column 'email' =>
 column 'tasty' =>
   type is 'boolean',
   is immutable;
+};
 
-
-package TestApp::Plugin::REST::Model::User;
-use base qw/TestApp::Plugin::REST::Record/;
 
 # Your model-specific methods go here.
 
