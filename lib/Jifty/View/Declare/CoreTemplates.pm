cbf2d7a6d987ea8c7b6709cf5703bc20db446432
Jesse Vincent <jesse@bestpractical.com>
2007-01-26 13:12:35 +0000

    * finish untangling templates and abstract base class
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/template-declare@2591 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/View/Declare/CoreTemplates.pm
+++ lib/Jifty/View/Declare/CoreTemplates.pm
@@ -3,38 +3,11 @@ package Jifty::View::Declare::CoreTemplates;
 use strict;
 use warnings;
 use vars qw( $r );
-use base qw/Jifty::View::Declare::Helpers/;
+
+use Jifty::View::Declare -base;
+
 use Scalar::Defer;
-use Template::Declare::Tags;
-use Jifty::View::Declare::Helpers;
 
-our @EXPORT = (
-    @Jifty::View::Declare::Helpers::EXPORT,
-    @Template::Declare::Tags::EXPORT,
-    qw( page ),
-);
-
-{
-    no warnings 'redefine';
-
-    sub show {
-        # Handle relative path here!
-
-        my $path = shift;
-        $path =~ s{^/}{};
-        Jifty::View::Declare::Helpers->can('show')->( $path, @_ );
-    }
-}
-
-# template 'foo' => page {{ title is 'Foo' } ... };
-sub page (&) {
-    my $code = shift;
-    sub {
-        Jifty->handler->apache->content_type('text/html; charset=utf-8');
-        show('/_elements/nav');
-        wrapper($code);
-    };
-}
 
 template '_elements/nav' => sub {
     my $top = Jifty->web->navigation;
