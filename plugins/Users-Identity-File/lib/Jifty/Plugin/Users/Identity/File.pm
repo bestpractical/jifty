8d6cad3e9c0826727751f16eb32a9c500b7e449c
Yves Agostini <agostini@univ-metz.fr>
2007-01-26 15:47:46 +0000

    * first shot (and not workable) for plugins 2.0 auth plugins
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@2611 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

---
+++ plugins/Users-Identity-File/lib/Jifty/Plugin/Users/Identity/File.pm
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+package Jifty::Plugin::Users::Identity::File;
+use base qw/Jifty::Plugin Jifty::Plugin::Users/;
+
+# Your plugin goes here.  If takes any configuration or arguments, you
+# probably want to override L<Jifty::Plugin/init>.
+
+# ?? TODO
+# in init load file name
+
+1;
