59c6f16169e21933e38cc07e93e018bde5399fc4
Audrey Tang <audreyt@audreyt.org>
2007-01-29 16:29:03 +0000

    * Versioned objects! (Well, at least for creation)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/virtual-models@2733 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Record/Versioned.pm
+++ lib/Jifty/Record/Versioned.pm
@@ -17,8 +17,68 @@ In your F<etc/config.yml>:
 
 package Jifty::Record::Versioned;
 use Jifty::Util;
+use Jifty::YAML;
 use Jifty::Record;
 use base 'Jifty::DBI::Record';
+
+use SVN::Fs;
+use SVN::Repos;
+use SVN::Simple::Edit;
+use File::Basename;
+use File::Path;
+use YAML::Syck 0.82;
+
+my ($repos, $fs, $uri);
+
+sub init_repos {
+    my $self = shift;
+    return ($repos, $fs, $uri) if $repos;
+
+    my $path = Jifty::Util->absolute_path('var/repos');
+
+    File::Path::rmtree([$path]);
+
+    if (!-d $path) {
+        Jifty::Util->make_path(dirname($path));
+        $repos = SVN::Repos::create($path, undef, undef, undef,
+                        {'fs-type' => $ENV{SVNFSTYPE} || 'fsfs',
+                            'bdb-txn-nosync' => '1',
+                            'bdb-log-autoremove' => '1'});
+    }
+    else {
+        our $REPOSPOOL = SVN::Pool->new;
+        $repos = SVN::Repos::open($path, $REPOSPOOL);
+    }
+
+    $uri = File::Spec->rel2abs( $path ) ;
+    $uri =~ s{^|\\}{/}g if ($^O eq 'MSWin32');
+    $uri = "file://$uri";
+
+    print STDERR "*** Created Subversion Repository:\n    $uri\n\n";
+
+    $fs = $repos->fs;
+
+    return ($repos, $fs, $uri) if $repos;
+}
+
+sub new_edit {
+    my $self = shift;
+    my ($repos, $fs, $uri) = $self->init_repos;
+
+    my $base = $fs->youngest_rev;
+    my $edit = SVN::Simple::Edit->new(
+        _editor => [
+            SVN::Repos::get_commit_editor(
+                $repos, $uri, '/', 'root', 'FOO', undef, # \&committed
+            )
+        ],
+        pool => SVN::Pool->new,
+        missing_handler => SVN::Simple::Edit::check_missing($fs->revision_root ($base))
+    );
+    $edit->open_root($base);
+
+    return $edit;
+}
 
 sub __create {
     my ($self, %attribs) = @_;
@@ -26,7 +86,20 @@ sub __create {
     my $rv = $self->SUPER::__create(%attribs);
     if ($rv) {
         # Write to SQL!
-        # print YAML::Syck::Dump(\%attribs);
+        my $edit= $self->new_edit;
+
+        local $YAML::Syck::Headless = 1;
+        local $YAML::Syck::ImplicitTyping = 1;
+        local $YAML::Syck::ImplicitBinary = 1;
+        local $YAML::Syck::ImplicitUnicode = 1;
+
+        foreach my $key (sort keys %attribs) {
+            next if $key eq '__uuid';
+            $edit->add_file("=/$uuid/$key");
+            $edit->modify_file("=/$uuid/$key", YAML::Syck::Dump($attribs{$key}));
+        }
+
+        $edit->close_edit;
     }
     return $rv;
 }
