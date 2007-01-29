63b5c22c8d73f0d51dd36601e1d18f9a57dd0030
Audrey Tang <audreyt@audreyt.org>
2007-01-29 15:05:48 +0000

    * Ditch Jifty::Handle::SVK in favour of the better-named Jifty::Record::Versioned.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/virtual-models@2730 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Record/Versioned.pm
+++ lib/Jifty/Record/Versioned.pm
@@ -20,4 +20,25 @@ use Jifty::Util;
 use Jifty::Record;
 use base 'Jifty::DBI::Record';
 
+sub __create {
+    my ($self, %attribs) = @_;
+    my $uuid = ($attribs{__uuid} ||= Jifty::Util->generate_uuid);
+    my $rv = $self->SUPER::__create(%attribs);
+    if ($rv) {
+        # Write to SQL!
+        print YAML::Syck::Dump(\%attribs);
+    }
+    return $rv;
+}
+
+sub __set {
+    my $self = shift;
+    return $self->SUPER::__set(@_);
+}
+
+sub __delete {
+    my $self = shift;
+    return $self->SUPER::__set(@_);
+}
+
 1;
