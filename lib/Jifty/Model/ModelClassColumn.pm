53e90414e6d1394390a1f44e882c185adecb0045
Jesse Vincent <jesse@bestpractical.com>
2007-02-11 01:42:28 +0000

    *now only failing SOME of the model tests (label_text fallout). broke the rest dispatcher though. probably related to the previous commit
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/virtual-models@2784 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Model/ModelClassColumn.pm
+++ lib/Jifty/Model/ModelClassColumn.pm
@@ -158,9 +158,8 @@ sub after_create {
     my $idref = shift;
     $self->load_by_cols(id => $$idref);
     $self->model_class->add_column($self);
-
     my $ret = Jifty->handle->simple_query( $self->model_class->qualified_class->add_column_sql( $self->name ) );
-    $ret or die "error updating a table: " . $ret->error_message;
+    $ret || $self->log->fatal( "error updating a table: " . $ret->error_message);
 }
 
 =head2 table
--- t/TestApp-DatabaseBackedModels/lib/TestApp/DatabaseBackedModels/Bootstrap.pm
+++ t/TestApp-DatabaseBackedModels/lib/TestApp/DatabaseBackedModels/Bootstrap.pm
@@ -9,9 +9,10 @@ sub run {
     $modelclass->create( name => 'Widget');
 
     my $col = Jifty::Model::ModelClassColumn->new(current_user => $user);
-    $col->create( name => 'name', label => 'Name', storage_type => 'text', hints => 'This is the widget name', model_class => $modelclass);
+    $col->create( name => 'name', label_text => 'Name', storage_type => 'text', hints => 'This is the widget name', model_class => $modelclass);
+    return 1;
        my $col2 = Jifty::Model::ModelClassColumn->new(current_user => $user);
-    $col2->create( name => 'inventory', label => 'Inventory', storage_type => 'int', hints => 'How many do we have on hand?', model_class => $modelclass);
+    $col2->create( name => 'inventory', label_text => 'Inventory', storage_type => 'int', hints => 'How many do we have on hand?', model_class => $modelclass);
 
     my $widget = TestApp::DatabaseBackedModels::Model::Widget->new(current_user => $user);
     $widget->create(name => 'Weeble', inventory => 6);
