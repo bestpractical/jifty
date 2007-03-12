016f92dab365729b60d151099d44f3f90019377c
Jesse Vincent <jesse@bestpractical.com>
2007-03-12 00:35:09 +0000

    * Sample usage
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@2944 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Bootstrap.pm
+++ lib/Jifty/Bootstrap.pm
@@ -11,6 +11,19 @@ C<Jifty::Bootstrap> is an abstract base class for your application's
 bootstrapping.  Use it to set up initial data in your database when
 your application is first installed.
 
+=head1 EXAMPLE
+
+ 
+ package MyApp::Bootstrap;
+ use base 'Jifty::Bootstrap';
+ 
+ sub run {
+     my $user = MyApp::CurrentUser->new( _bootstrap => 1);
+     my $modelclass = MyApp::Model::Thingy->new(current_user => $user);
+     $modelclass->create( name => 'Widget');
+ }; 
+ 
+ 
 =cut
 
 package Jifty::Bootstrap;
