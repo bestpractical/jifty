809b7680d8abf3fbe6f9af62d47427269d1001a5
Jesse Vincent <jesse@bestpractical.com>
2007-03-14 03:12:44 +0000

    * No longer need this hackery
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@2963 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/View/Declare/BaseClass.pm
+++ lib/Jifty/View/Declare/BaseClass.pm
@@ -2,29 +2,15 @@ package Jifty::View::Declare::BaseClass;
 
 use strict;
 use warnings;
-use vars qw( $r );
 use base qw/Exporter Jifty::View::Declare::Helpers/;
 use Scalar::Defer;
 use Template::Declare::Tags;
+
+
 use Jifty::View::Declare::Helpers;
 
-our @EXPORT = (
-    @Jifty::View::Declare::Helpers::EXPORT,
-    @Template::Declare::Tags::EXPORT,
-);
 
-{
-    no warnings 'redefine';
-
-    sub show {
-        # Handle relative path here!
-
-        my $path = shift;
-        $path =~ s{^/}{};
-        Jifty::View::Declare::Helpers->can('show')->( $path, @_ );
-    }
-}
-
+our @EXPORT = ( @Jifty::View::Declare::Helpers::EXPORT);
 
 
 1;
