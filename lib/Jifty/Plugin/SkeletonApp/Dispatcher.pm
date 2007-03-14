9f886323202f40011aa7462c7346b903ea719825
Jesse Vincent <jesse@bestpractical.com>
2007-03-14 03:11:19 +0000

    * skeleton app fleshing out
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@2956 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/SkeletonApp/Dispatcher.pm
+++ lib/Jifty/Plugin/SkeletonApp/Dispatcher.pm
@@ -16,6 +16,28 @@ When a user asks for /, give them index.html.
 
 use Jifty::Dispatcher -base;
 
+    
+
+on '*' => run {
+    my $top = Jifty->web->navigation;
+    $top->child( Home => url => "/", sort_order => 1, label => _('Home') );
+    if ( Jifty->config->framework('AdminMode') ) {
+        $top->child(
+            Administration =>
+              url          => "/__jifty/admin/",
+            label      => _('Administration'),
+            sort_order => 998
+        );
+        $top->child(
+            OnlineDocs =>
+              url      => "/__jifty/online_docs/",
+            label      => _('Online docs'),
+            sort_order => 999
+        );
+    }
+    return ();
+};
+
 on '/' => run { show 'index.html' };
 
 1;
--- lib/Jifty/Plugin/SkeletonApp/View.pm
+++ lib/Jifty/Plugin/SkeletonApp/View.pm
@@ -20,28 +20,9 @@ Jifty's "pony" Application. It could certainly use some refactoring. (And some o
 
 =cut
 
-template '_elements/nav' => sub {
-    my $top = Jifty->web->navigation;
-    $top->child( Home => url => "/", sort_order => 1, label => _('Home') );
-    if ( Jifty->config->framework('AdminMode') ) {
-        $top->child(
-            Administration =>
-              url          => "/__jifty/admin/",
-            label      => _('Administration'),
-            sort_order => 998
-        );
-        $top->child(
-            OnlineDocs =>
-              url      => "/__jifty/online_docs/",
-            label      => _('Online docs'),
-            sort_order => 999
-        );
-    }
-    return ();
-};
-
-template '_elements/sidebar' => sub {
-    with( id => "salutation" ), div {
+private template 'salutation' => sub {
+    div {
+    attr {id => "salutation" };
         if (    Jifty->web->current_user->id
             and Jifty->web->current_user->user_object )
         {
@@ -52,10 +33,13 @@ template '_elements/sidebar' => sub {
         else {
             _("You're not currently signed in.");
         }
-    };
-    with( id => "navigation" ), div {
-        Jifty->web->navigation->render_as_menu;
-    };
+    }
+};
+
+private template 'menu' => sub {
+    div {
+    attr { id => "navigation" };
+        Jifty->web->navigation->render_as_menu; };
 };
 
 template '__jifty/empty' => sub {
@@ -63,36 +47,29 @@ template '__jifty/empty' => sub {
 };
 
 
-template '_elements/header' => sub {
+private template 'header' => sub {
     my ($title) = get_current_attr(qw(title));
     Jifty->handler->apache->content_type('text/html; charset=utf-8');
-    head {
+    head { 
         with(
             'http-equiv' => "content-type",
             content      => "text/html; charset=utf-8"
-          ),
+          ),    
           meta {};
         with( name => 'robots', content => 'all' ), meta {};
         title { _($title) };
-
         Jifty->web->include_css;
         Jifty->web->include_javascript;
-      }
+      };
+
 };
 
 
-template '_elements/keybindings' => sub {
+
+private template 'keybindings' => sub {
     div { id is "keybindings" };
 };
 
-template 'index.html' => page {
-    { title is _('Welcome to your new Jifty application') }
-    img {
-        src is "/static/images/pony.jpg", alt is _(
-            'You said you wanted a pony. (Source %1)',
-            'http://hdl.loc.gov/loc.pnp/cph.3c13461'
-        );
-    };
-};
+#template 'index.html' => page { { title is _('Welcome to your new Jifty application') } img { src is "/static/images/pony.jpg", alt is _( 'You said you wanted a pony. (Source %1)', 'http://hdl.loc.gov/loc.pnp/cph.3c13461'); }; };
 
 1;
