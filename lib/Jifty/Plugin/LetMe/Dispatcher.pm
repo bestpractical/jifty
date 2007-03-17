4d0b77733e4f47ff8e32d4b2c4aea97a5a9adf29
Jesse Vincent <jesse@bestpractical.com>
2007-03-17 05:45:15 +0000

    cleaned up the dispatcher for letme
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@3007 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/LetMe/Dispatcher.pm
+++ lib/Jifty/Plugin/LetMe/Dispatcher.pm
@@ -19,17 +19,15 @@ before qr'^/let/(.*)' => run {
     my $let_me = Jifty::LetMe->new();
     $let_me->from_token($1);
     redirect '/error/let_me/invalid_token' unless $let_me->validate;
+    warn ref( $let_me->validated_current_user );
+    Jifty->web->temporary_current_user( $let_me->validated_current_user );
 
-    Jifty->web->temporary_current_user($let_me->validated_current_user);
-
-    my %args = %{$let_me->args};
+    my %args = %{ $let_me->args };
     set $_ => $args{$_} for keys %args;
     set let_me => $let_me;
 };
 
-on qr'^/let/' => run {
-    my $let_me = get 'let_me';
-    show '/let/' . $let_me->path;
-};
+on qr'^/let/' =>
+    run { my $let_me = get 'let_me'; show '/let/' . $let_me->path; };
 
 1;
