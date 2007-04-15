1d48e2e4d9fdccf7b8170ebeebe8a1f72a338a5f
Jesse Vincent <jesse@bestpractical.com>
2007-04-15 16:55:47 +0000

    * Require current T::D
    * Bump for release
    
    * Require new Jifty::DBI
    
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@3134 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty.pm
+++ lib/Jifty.pm
@@ -11,7 +11,7 @@ BEGIN {
     require Time::Local;
 
     # Declare early to make sure Jifty::Record::schema_version works
-    $Jifty::VERSION = '0.70117'; 
+    $Jifty::VERSION = '0.70415'; 
 }
 
 =head1 NAME
