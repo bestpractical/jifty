347196367196f6c27ce4a886cef98b32c524dc8e
Chia-liang Kao <clkao@bestpractical.com>
2007-06-01 19:58:34 +0000

    openid related methods.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@3315 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/OpenID/Mixin/Model/User.pm
+++ lib/Jifty/Plugin/OpenID/Mixin/Model/User.pm
@@ -18,4 +18,47 @@ column openid =>
 
 sub has_alternative_auth { 1 }
 
+
+use URI;
+
+sub validate_openid {
+    my $self   = shift;
+    my $openid = shift;
+
+    my $uri = URI->new( $openid );
+
+    return ( 0, q{That doesn't look like an OpenID URL.} )
+        if not defined $uri;
+
+    my $temp_user = Doxory::Model::User->new;
+    $temp_user->load_by_cols( openid => $uri->canonical );
+
+    # It's ok if *we* have the openid we're looking for
+    return ( 0, q{It looks like somebody else has claimed that OpenID.} )
+        if $temp_user->id and ( not $self->id or $temp_user->id != $self->id );
+
+    return 1;
+}
+
+sub canonicalize_openid {
+    my $self   = shift;
+    my $openid = shift;
+
+    return ''
+        if not defined $openid or not length $openid;
+
+    $openid = 'http://' . $openid
+        if $openid !~ m{^http://};
+
+    my $uri = URI->new( $openid );
+
+    return $uri->canonical;
+}
+
+sub link_to_openid {
+    my $self   = shift;
+    my $openid = shift;
+    $self->__set( column => 'openid', value => $openid );
+}
+
 1;
