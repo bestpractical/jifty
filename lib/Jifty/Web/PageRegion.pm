e4c6e23297fbc6db137e021c28076dea4d19dda6
Chia-liang Kao <clkao@bestpractical.com>
2007-06-04 18:58:46 +0000

    Move methods unrelated to mason to Jifty::View.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@3348 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Web/PageRegion.pm
+++ lib/Jifty/Web/PageRegion.pm
@@ -298,7 +298,7 @@ sub as_string {
 sub render_as_subrequest {
     my ($self, $out_method, $arguments, $enable_actions) = @_;
 
-    my $orig_out = Jifty->handler->mason->interp->out_method || \&Jifty::View::Mason::Handler::out_method;
+    my $orig_out = Jifty->handler->mason->interp->out_method || Jifty::View->can('out_method');
 
     Jifty->handler->mason->interp->out_method($out_method);
 
