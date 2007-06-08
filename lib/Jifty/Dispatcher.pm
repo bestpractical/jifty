d7bc5f16209b22ca9bd0d8fe9167d932294c3398
Chia-liang Kao <clkao@bestpractical.com>
2007-06-08 07:02:46 +0000

    Prevent infinite loop when unable to render mason_internal_error.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/fragcont@3398 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Dispatcher.pm
+++ lib/Jifty/Dispatcher.pm
@@ -1149,6 +1149,7 @@ sub render_template {
     my $self     = shift;
     my $template = shift;
     my $showed   = 0;
+#    local $@;
     eval {
         foreach my $handler ( Jifty->handler->view_handlers ) {
             if ( Jifty->handler->view($handler)->template_exists($template) ) {
@@ -1167,7 +1168,11 @@ sub render_template {
 
     # Handle parse errors
     if ( $err and not eval { $err->isa('HTML::Mason::Exception::Abort') } ) {
-
+        if ($template eq '/__jifty/error/mason_internal_error') {
+            $self->log->debug("can't render internal_error: $err");
+            $self->_abort;
+            return;
+        }
         # Save the request away, and redirect to an error page
         Jifty->web->response->error($err);
         my $c = Jifty::Continuation->new(
@@ -1175,8 +1180,6 @@ sub render_template {
             response => Jifty->web->response,
             parent   => Jifty->web->request->continuation,
         );
-
-        warn "$err";
 
         # Redirect with a continuation
         Jifty->web->_redirect( "/__jifty/error/mason_internal_error?J:C=" . $c->id );
