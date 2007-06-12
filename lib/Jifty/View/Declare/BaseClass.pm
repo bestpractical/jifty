2476bf1556b3902df441d260bb31d56a7de18c2a
Chia-liang Kao <clkao@bestpractical.com>
2007-06-12 19:14:08 +0000

    YourApp::View->use_mason_wrapper now lets your write page {} in TD but uses your mason wrapper.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@3456 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/View/Declare/BaseClass.pm
+++ lib/Jifty/View/Declare/BaseClass.pm
@@ -13,6 +13,31 @@ use Jifty::View::Declare::Helpers;
 our @EXPORT = ( @Jifty::View::Declare::Helpers::EXPORT);
 
 
+sub use_mason_wrapper {
+    my $class = shift;
+    no strict 'refs';
+    *{ $class . '::wrapper' } = sub ($) {
+        my $code = shift;
+	no warnings 'redefine';
+        # so in td handler, we made jifty::web->out appends to td
+        # buffer, we need it back for here, because $code is actually
+        # called.  someday we need to finish fixing the output system
+        # that is in Jifty::View.
+        my $td_out = \&Jifty::Web::out;
+        local *Jifty::Web::out = sub { shift->mason->out(@_) };
+
+        Jifty->handler->fallback_view_handler->interp->autoflush(1);
+        local *HTML::Mason::Request::content
+            = sub { local *Jifty::Web::out = $td_out; $code; '' };
+        Jifty->web->request->arguments->{title} = 'YADA!';
+        Jifty->handler->fallback_view_handler->show('/_elements/wrapper');
+    }
+}
+
+=cut
+
+
+
 1;
 __DATA__
 
@@ -26,6 +51,11 @@ This class provides a baseclass for your C<Template::Declare> derived view class
 
 
 =head1 METHODS
+
+=head2 use_mason_wrapper
+
+Call this function in your viewclass to use your mason wrapper for TD templates.
+
 
 =head2 show templatename arguments
 
