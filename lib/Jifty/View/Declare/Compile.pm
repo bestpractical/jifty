46d874a7d2c2c860113d6ea9a20ee5f4bef7b4b6
Chia-liang Kao <clkao@bestpractical.com>
2007-06-17 20:18:32 +0000

    handle enteriter.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/trimclient@3512 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/View/Declare/Compile.pm
+++ lib/Jifty/View/Declare/Compile.pm
@@ -18,6 +18,9 @@ sub binop {
     return $ret;
 
 }
+
+sub pp_leaveloop { shift->loop_common(@_, "") }
+
 package Jifty::View::Declare::Compile;
 use strict;
 use base 'B::Deparse';
@@ -34,6 +37,11 @@ use B qw(class main_root main_start main_cv svref_2object opnumber perlstring
 	 PMf_MULTILINE PMf_SINGLELINE PMf_FOLD PMf_EXTENDED);
 
 use constant method_invocation => '.';
+
+sub is_state {
+    goto \&B::Deparse::is_state;
+}
+*null = B::Deparse::null;
 
 sub padname {
     my $self = shift;
@@ -52,6 +60,45 @@ sub deparse {
     my $ret = $self->SUPER::deparse(@_);
     return '' if $ret =~ m/use (strict|warnings)/;
     return $ret;
+}
+
+sub loop_common {
+    my $self = shift;
+    my($op, $cx, $init) = @_;
+    my $enter = $op->first;
+    my $kid = $enter->sibling;
+    if ($enter->name eq "enteriter") { # foreach
+	my $ary = $enter->first->sibling; # first was pushmark
+	my $var = $ary->sibling;
+
+	if ($ary->name eq 'null' and $enter->private & OPpITER_REVERSED) {
+	    # "reverse" was optimised away
+	    return $self->SUPER::loop_common(@_);
+	} elsif ($enter->flags & OPf_STACKED
+	    and not null $ary->first->sibling->sibling)
+	{
+	    return $self->SUPER::loop_common(@_);
+	} else {
+	    $ary = $self->deparse($ary, 1);
+	}
+
+	if ($var->name ne "gv") {
+	    return $self->SUPER::loop_common(@_);
+	}
+
+	$var = '$' . $self->deparse($var, 1);
+
+	my $body = $kid->first->first->sibling; # skip OP_AND and OP_ITER
+	# statement() foreach (@foo);
+	if (!is_state $body->first and $body->first->name ne "stub") {
+	    Carp::confess unless $var eq '$_';
+	    $body = $body->first;
+	    return "$ary.each(function (\$_) {".$self->deparse($body, 2)."} )";
+	}
+	# XXX not handling cont block here yet
+	return "$ary.each(function (\$_) {".$self->deparse($body, 0)."} )";
+    }
+    return $self->SUPER::loop_common(@_);
 }
 
 sub maybe_my {
