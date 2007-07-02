4d73b87ff9ca4849ec3b138065e87af21e3f6fdc
Andrew Sterling Hanenkamp <sterling@hanenkamp.com>
2007-07-02 00:35:12 +0000

    Adding documentation to Jifty::Plugin::Debug to fix POD coverage failures.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@3591 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/Debug.pm
+++ lib/Jifty/Plugin/Debug.pm
@@ -4,4 +4,14 @@ use warnings;
 package Jifty::Plugin::Debug;
 use base qw/Jifty::Plugin/;
 
+=head1 NAME
+
+Jifty::Plugin::Debug - a plugin to log each incoming request
+
+=head1 DESCRIPTION
+
+Enable this plugin in your F<etc/config.yml> (requires no configuration) and the plugin add an INFO level log message on each request received. It will contain the PID of the current process, the URL requested, and the username (if any) of the person making the request.
+
+=cut
+
 1;
