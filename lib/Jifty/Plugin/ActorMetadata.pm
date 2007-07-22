6c59063c19befff7337831fd59e345758f63a9a1
Jesse Vincent <jesse@bestpractical.com>
2007-07-22 20:22:02 +0000

    * added some pod to help make pod tests pass
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@3708 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/ActorMetadata.pm
+++ lib/Jifty/Plugin/ActorMetadata.pm
@@ -4,4 +4,30 @@ use warnings;
 package Jifty::Plugin::ActorMetadata;
 use base qw/Jifty::Plugin/;
 
+=head1 NAME
+
+Jifty::Plugin::ActorMetadata
+
+=head1 DESCRIPTION
+ 
+This plugin adds a model mixin which adds C<created_by>, C<created_on> and C<updated_on> columns to a model class.
+
+=head1 EXAMPLE 
+
+use strict;
+ use warnings;
+ 
+ package MeetMeow::Model::Cat;
+ use Jifty::DBI::Schema;
+ 
+ use MeetMeow::Record schema {
+ 
+         ...
+ 
+ };
+ use Jifty::Plugin::ActorMetadata::Mixin::Model::ActorMetadata; # created_by, created_on, updated_on
+ 
+
+=cut
+
 1;
