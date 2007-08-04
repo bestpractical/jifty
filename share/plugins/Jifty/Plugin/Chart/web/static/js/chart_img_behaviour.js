ca76eac7a30458c66b9dc6dfd176bff3040987c6
Andrew Sterling Hanenkamp <sterling@hanenkamp.com>
2007-08-04 22:32:26 +0000

    Merge down from trunk.  r8333@dynpc145:  andrew | 2007-08-03 16:30:37 -0500  Fix failing dependency test because Chart uses GD to fix testing.  r8334@dynpc145:  andrew | 2007-08-03 16:31:13 -0500  Updated the behaviour script used by IMG chart renderers to make it more URI aware.  r8351@dynpc145:  andrew | 2007-08-04 16:13:23 -0500  Standardizing the chart types across the three current renderers.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/virtual-models@3793 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- share/plugins/Jifty/Plugin/Chart/web/static/js/chart_img_behaviour.js
+++ share/plugins/Jifty/Plugin/Chart/web/static/js/chart_img_behaviour.js
@@ -8,9 +8,26 @@ Behaviour.register({
     'img.chart': function(e) {
         var dim = Element.getDimensions(e);
         var url = e.src;
-        url += url.indexOf('?') >= 0 ? '&' : '?';
-        url += 'width=' + dim.width + 'px';
-        url += '&height=' + dim.height + 'px';
+
+        var path  = url;
+        var query = $H();
+
+        if (url.indexOf('?') >= 0) {
+            var path_and_query = url.split('?');
+            path = path_and_query[0];
+
+            var query_params = path_and_query[1].split('&');
+            for (var query_param in query_params) {
+                var key_and_value = query_param.split('=');
+                query[ key_and_value[0] ] = key_and_value[1];
+            }
+        }
+
+        query['width']  = dim.width + 'px';
+        query['height'] = dim.height + 'px';
+
+        url = path + '?' + query.toQueryString();
+
         e.src = url;
     },
 });
