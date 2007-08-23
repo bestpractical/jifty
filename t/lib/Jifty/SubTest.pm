2a3721150f8a79838dfb940bf296569d4c316cb2
Jesse Vincent <jesse@bestpractical.com>
2007-08-23 18:02:33 +0000

    * Remove cargo-culting changing of $0 from Jifty::SubTest. It doesn't affect test output but does break things
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@3947 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- t/lib/Jifty/SubTest.pm
+++ t/lib/Jifty/SubTest.pm
@@ -4,7 +4,6 @@ use FindBin;
 use File::Spec;
 BEGIN {
     @INC = grep { defined } map { ref($_) ? $_ : File::Spec->rel2abs($_) } @INC;
-    $0 = File::Spec->rel2abs($0);
     chdir "$FindBin::Bin/..";
 }
 
