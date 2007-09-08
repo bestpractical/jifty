bd8246038c58af1845edb3f9e53c61109478cf1f
Chia-liang Kao <clkao@bestpractical.com>
2007-09-08 13:42:57 +0000

    first cut of tests for singlepage app plugin.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@4062 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

---
+++ t/TestApp-Plugin-SinglePage/lib/TestApp/Plugin/SinglePage/Model/User.pm
@@ -0,0 +1,14 @@
+package TestApp::Plugin::SinglePage::Model::User;
+use warnings;
+use strict;
+use base qw/TestApp::Plugin::SinglePage::Record/;
+use Jifty::DBI::Schema;
+
+
+use TestApp::Plugin::SinglePage::Record schema {
+};
+
+use Jifty::Plugin::User::Mixin::Model::User;
+
+1;
+
---
+++ t/TestApp-Plugin-SinglePage/lib/TestApp/Plugin/SinglePage/View.pm
@@ -0,0 +1,45 @@
+package TestApp::Plugin::SinglePage::View;
+use strict;
+use Jifty::View::Declare -base;
+use Jifty::View::Declare::Helpers;
+
+
+template '/index.html' =>
+    page { title => 'test' }
+    content {
+	with (submit_to => '/page2'),
+	form {
+	    select { { name is 'foo' };
+		     option { { value is $_ }; $_ } for 1..10;
+		 };
+	    form_submit( label => 'Next' );
+	};
+	render_region('random');
+    };
+
+template '/page2' =>
+    page { title => 'page2' }
+    content {
+	my $foo = get('foo');
+	my $create = Jifty->web->new_action( class => 'CreateUser', moniker => 'create_user',
+					     arguments => { name => $foo });
+	my $redir = new_action(class     => "Jifty::Action::Redirect",
+			       arguments => { url => '/index.html' });
+	form {
+	    Jifty->web->form->register_action($redir);
+	    render_action($create);
+	    form_submit( label   => 'Next' );
+	}
+    };
+
+template 'page3' =>
+    page { title => 'page3' }
+    content {
+	h1 { 'yatta' };
+	form {
+	    hyperlink(label => 'Back', url => '/');
+	}
+    };
+
+1;
+
