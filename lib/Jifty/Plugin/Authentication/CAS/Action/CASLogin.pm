34b602a11e415aa784bc02f5f700cb677c0b52fc
Yves Agostini <agostini@univ-metz.fr>
2007-11-30 19:37:56 +0000

    allow authentication by mail or cas login and domain
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@4583 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/Authentication/CAS/Action/CASLogin.pm
+++ lib/Jifty/Plugin/Authentication/CAS/Action/CASLogin.pm
@@ -87,9 +87,14 @@ sub take_action {
       return;
     };
      
+    my ($name,$email);
+    #TODO add a ldap conf to find name and email
+    $email = $username.'@'.$plugin->domain() if ($plugin->domain());
+
     # Load up the user
     my $current_user = Jifty->app_class('CurrentUser');
-    my $user = $current_user->new( cas_id => $username );
+    my $user = ($email) ? $current_user->new( email => $email)    # load by email to mix authentication
+                        : $current_user->new( cas_id => $username );  # else load by cas_id
 
     # Autocreate the user if necessary
     if ( not $user->id ) {
@@ -113,12 +118,10 @@ sub take_action {
 
     my $u = $user->user_object;
 
-    my ($name,$email);
-    #TODO add a ldap conf to find name and email
-    $email = $username.'@'.$plugin->domain() if ($plugin->domain());
-    $u->__set( column => 'name', value => $username ) if (!$u->name);
 
     # Update, just in case
+    $u->__set( column => 'cas_id', value => $username ) if (!$u->cas_id);
+    $u->__set( column => 'name', value => $username ) if (!$u->name);
     $u->__set( column => 'name', value => $name ) if ($name);
     $u->__set( column => 'email', value => $email ) if ($email);
  
