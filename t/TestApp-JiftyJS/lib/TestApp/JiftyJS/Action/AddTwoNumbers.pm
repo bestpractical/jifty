acff640383d696d0f881f6985c019c708faed375
Kang-min Liu <gugod@gugod.org>
2007-12-14 07:38:57 +0000

    For testing continuation, add a "AddTwoNumber" wizard like the one in Jifty::Manual::Continuations, only it's written with TD rather then mason template.
    To manually test it, goto /c/page1 first. Type some number and hit
    enter. You sholud then visit /c/page2. Type some other number and hit
    enter. You should return to /c/page1 with the result of AddTwoNumber
    action shown in the message box.
    
    
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@4686 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

---
+++ t/TestApp-JiftyJS/lib/TestApp/JiftyJS/Action/AddTwoNumbers.pm
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+
+=head1 NAME
+
+TestApp::JiftyJS::Action::AddTwoNumbers
+
+=cut
+
+package TestApp::JiftyJS::Action::AddTwoNumbers;
+use base qw/TestApp::JiftyJS::Action Jifty::Action/;
+
+use Jifty::Param::Schema;
+use Jifty::Action schema {
+    param first_number  => type is 'integer', render as 'Text';
+    param second_number => type is 'integer', render as 'Text';
+};
+
+=head2 take_action
+
+=cut
+
+sub take_action {
+    my $self = shift;
+    my $one = $self->argument_value("first_number");
+    my $two = $self->argument_value("second_number");
+    $self->result->message("Got " . ($one + $two));
+    return 1;
+}
+
+1;
--- t/TestApp-JiftyJS/lib/TestApp/JiftyJS/View.pm
+++ t/TestApp-JiftyJS/lib/TestApp/JiftyJS/View.pm
@@ -61,4 +61,31 @@ template 'region2' => sub {
     p { "Region Two" }
 };
 
+
+# Templtes for testing continuation. Using the example in Jifty::Manual::Continuations
+template '/c/page1' => page {
+    my $action = new_action(class => 'AddTwoNumbers');
+    form {
+        $action->form_field( 'first_number' )->render;
+        $action->form_field( 'second_number',
+                             default_value => {
+                                 request_argument => "number",
+                             }
+                         )->render;
+        tangent(
+            url => '/c/page2',
+            submit => $action,
+            label => "Enter a second number"
+        );
+    };
+};
+
+template '/c/page2' => page {
+    form {
+        label { "Second Number" };
+        outs_raw('<input type="text" name="number" />');
+        form_return( label => "Pick", as_button => 1);
+    }
+};
+##
 1;
