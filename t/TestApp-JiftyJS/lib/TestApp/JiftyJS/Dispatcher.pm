4ef77cdc7c9056ed812de6e445558ca2b86d13a6
Kang-min Liu <gugod@gugod.org>
2008-02-17 02:48:07 +0000

    Allow all the action for testing purpose.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/branches/jquery@5126 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- t/TestApp-JiftyJS/lib/TestApp/JiftyJS/Dispatcher.pm
+++ t/TestApp-JiftyJS/lib/TestApp/JiftyJS/Dispatcher.pm
@@ -1,10 +1,13 @@
+
 package TestApp::JiftyJS::Dispatcher;
+use Jifty::Dispatcher -base;
 use strict;
 use warnings;
-use Jifty::Dispatcher -base;
 
 before '*' => run {
     Jifty->api->allow("AddTwoNumbers");
+    Jifty->api->allow('Play');
+    Jifty->api->allow('Play2');
 };
 
 1;
