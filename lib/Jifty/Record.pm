4c41c5e85d244ab1cb6cd58f7bf2dfa8de4425ff
Dobrica Pavlinusic <dpavlin@rot13.org>
2008-03-18 21:21:11 +0000

    revert load_or_create canonicalization and move discussion on jifty-devel
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@5230 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Record.pm
+++ lib/Jifty/Record.pm
@@ -143,13 +143,6 @@ sub load_or_create {
 
     my %args = (@_);
 
-    foreach my $key ( keys %args ) {
-        $args{$key} = $self->run_canonicalization_for_column(
-            column => $key,
-            value  => $args{$key}
-        );
-    }
-
     my ( $id, $msg ) = $self->load_by_cols(%args);
     unless ( $self->id ) {
         return $self->create(%args);
