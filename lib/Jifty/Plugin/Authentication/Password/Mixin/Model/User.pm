f01990e853fc5d97075bcd33a661fffbea8a27e9
Ruslan Zakirov <ruz@bestpractical.com>
2008-05-16 14:29:33 +0000

    typo
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@5469 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/Authentication/Password/Mixin/Model/User.pm
+++ lib/Jifty/Plugin/Authentication/Password/Mixin/Model/User.pm
@@ -18,7 +18,7 @@ Jifty::Plugin::Authentication::Password::Mixin::Model::User - password plugin us
   package MyApp::Model::User;
   use Jifty::DBI::Schema;
   use MyApp::Record schema {
-      # custom column defrinitions
+      # custom column definitions
   };
 
   use Jifty::Plugin::User::Mixin::Model::User; # name, email, email_confirmed
