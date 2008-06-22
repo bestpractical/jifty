699d1532fc2edad1fb5f6ae5cdd454c44e43ed42
Chia-liang Kao <clkao@bestpractical.com>
2008-06-22 14:22:04 +0000

    upgrade jquery to 1.2.6.  This removes the patch we had for script evaluation bug in IE6.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@5520 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Web.pm
+++ lib/Jifty/Web.pm
@@ -38,8 +38,7 @@ __PACKAGE__->javascript_libs([qw(
     jsan/Upgrade/Array/push.js
     jsan/DOM/Events.js
     json.js
-    jquery-1.2.3.js
-    jquery_patch.js
+    jquery-1.2.6.js
     iutil.js
     iautocompleter.js
     jifty_interface.js
