aa6b455919336e600ec0c7b83e5b3970a2dd6a46
Liang-Bin Hsueh <hlb@bestpractical.com>
2008-08-19 17:44:50 +0000

    rewrite SinglePage plugin:
    * it does not append #spa_start any longer.
    * it does not force browsers to reload page when users pastes URL in location bar, nor clicks back button from other sites.
    * simplified logic.
    
    
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@5757 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- share/plugins/Jifty/Plugin/SinglePage/web/static/js/singlepage/spa.js
+++ share/plugins/Jifty/Plugin/SinglePage/web/static/js/singlepage/spa.js
@@ -1,20 +1,23 @@
+window.dhtmlHistory.create({
+    toJSON: function(o) {
+        return JSON.stringify(o);
+    }
+    , fromJSON: function(s) {
+        return JSON.parse(s);
+    }
+});
+
 (function($) {
     SPA = {
-        initialHash: "spa_start",
-        currentHash: null,
-        currentLocation: null,
+        initialHash: null,
         historyChange: function(newLocation, historyData, first) {
-
-            /* reload if user goes to the first page */
-            if (newLocation == SPA.initialHash) {
-                location.href = location.pathname + location.search;
-            }
-
             if (first) {
                 dhtmlHistory.add(newLocation, historyData);
             } else {
                 if (historyStorage.hasKey(newLocation)) {
                     Jifty.update(historyStorage.get(newLocation), "");
+                } else {
+                    Jifty.update({ "continuation": {}, "actions": {}, "fragments": [ { "mode": "Replace", "args": {}, "region": "__page", "path": SPA.initialHash } ], "action_arguments":{} }, "");
                 }
             }
         },
@@ -64,34 +67,10 @@
         }
     };
 
-    /*
-     * If user paste /#/abc in location bar, or click the reload button,
-     * then we should redirect him to the right page
-     */
-    SPA.currentHash = location.hash;
-    if (SPA.currentHash.length) {
-        if (SPA.currentHash.charAt(0) == '#' && SPA.currentHash.charAt(1) == '/') {
-            SPA.currentLocation = SPA.currentHash.slice(1);
-            location.href = SPA.currentLocation;
-        }
-    }
-
     $(document).ready(function(){
+        SPA.initialHash = location.pathname + location.search;
         dhtmlHistory.initialize();
         dhtmlHistory.addListener(SPA.historyChange);
-        if (dhtmlHistory.isFirstLoad()) {
-            dhtmlHistory.add(SPA.initialHash, "");
-        }
     });
     
 })(jQuery);
-
-
-window.dhtmlHistory.create({
-    toJSON: function(o) {
-        return JSON.stringify(o);
-    }
-    , fromJSON: function(s) {
-        return JSON.parse(s);
-    }
-});
