c303cbdc55daecba33001c8dc4537ebd317694c6
Chia-liang Kao <clkao@bestpractical.com>
2008-09-26 16:16:46 +0000

    - Create branch bulk-action
    first cut of the bulk action.
    
    
    test app for bulk action
    
    quick kludge to make basic bulk action work.
    
    
    minor refactoring.
    
    
    perform non-triggering actions properly.
    
    update comments
    
    automatic record_class detection.
    
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@5886 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

---
+++ lib/Jifty/Action/Record/Bulk.pm
@@ -0,0 +1,108 @@
+package Jifty::Action::Record::Bulk;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Jifty::Action::Record::Bulk - Perform multiple record actions
+
+=head1 SYNOPSIS
+
+  use strict;
+  use warnings;
+
+  package MyApp::Action::BulkUpdateFoo;
+  use base qw/ Jifty::Action::Record::Bulk /;
+
+  use constant record_class => 'MyApp::Model::Foo';
+
+  __PACKAGE__->add_action('MyApp::Action::DeleteFoo' => { trigger => 'delete', final => 1 });
+  __PACKAGE__->add_action('MyApp::Action::UpdateFoo');
+
+=head1 DESCRIPTION
+
+=cut
+
+use base qw/Jifty::Action::Record Class::Data::Inheritable/;
+
+__PACKAGE__->mk_classdata( actions => [] );
+__PACKAGE__->mk_classdata( record_class => undef );
+
+use constant ids_name => 'ids';
+
+sub add_action {
+    my ($class, $name, $param) = @_;
+    push @{$class->actions}, [ $name, $param ];
+    Jifty::Util->require($name);
+    if ($class->record_class) {
+        die "$class is not a action of @{[ $class->record_class ]}"
+            unless $class->record_class eq $name->record_class;
+    }
+    else {
+        $class->record_class( $name->record_class );
+    }
+}
+
+sub arguments {
+    my $self = shift;
+    my $arguments = { $self->ids_name => { render_as => 'text', sort_order => -999 } };
+
+    # composite of the arguments from all actions, and remove the pk
+    require Jifty::Param::Schema;
+
+    for (@{$self->actions}) {
+        my ($action_class, $param) = @$_;
+        $arguments = Jifty::Param::Schema::merge_params( $arguments, $action_class->can('arguments')->($self) );
+        delete $arguments->{id};
+    }
+
+    if ( $self->can('PARAMS') ) {
+        $arguments = Jifty::Param::Schema::merge_params(
+            $arguments, ($self->PARAMS || {})
+        );
+    }
+    return $arguments;
+}
+
+sub perform_action {
+    my ($self, $action_class, $ids) = @_;
+    for (@$ids) {
+        my $record = $self->record_class->new;
+        $record->load($_);
+        $self->record( $record );
+        $action_class->can('take_action')->($self);
+    }
+    # allow bulk action to define if they allow individual action to fail
+}
+
+sub take_action {
+    my $self = shift;
+    my $ids = $self->argument_value('ids');
+    $ids = [split /,/,$ids] if !ref($ids);
+    for (@{$self->actions}) {
+        my ($action_class, $param) = @$_;
+        # XXX: create real action objects and invoke them, so we have separate result objects
+
+        # $action_class->new( record => $loaded, arguments => ..., moniker => ... );
+        # collect action->result object for reporting, index by content of the current action
+
+        if (my $trigger = $param->{trigger}) {
+            if ($self->argument_value($trigger)) {
+                $self->perform_action($action_class, $ids);
+                last if $param->{final};
+            }
+        }
+        else {
+            $self->perform_action($action_class, $ids);
+        }
+    }
+}
+
+sub report_success {
+    my $self = shift;
+    $self->result->message(_("yatta"));
+}
+
+1;
+
---
+++ t/TestApp-Bulk/lib/TestApp/Bulk/View.pm
@@ -0,0 +1,7 @@
+package TestApp::Bulk::View;
+use warnings;
+use strict;
+
+use Jifty::View::Declare -base;
+
+1;
