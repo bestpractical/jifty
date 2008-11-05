d93bfb8055e9899bcd2746497050763f277f7ba4
Alex Vandiver <alexmv@bestpractical.com>
2008-11-05 22:40:22 +0000

    * Closing STDOUT causes Pg to hate us? (!)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@5982 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Dispatcher.pm
+++ lib/Jifty/Dispatcher.pm
@@ -861,7 +861,11 @@ sub _do_dispatch {
     }
 
     # Close the handle down, so the client can go on their merry way
-    close STDOUT unless Jifty->web->request->is_subrequest;
+    # XXX TODO FIXME: Doing this causes tests to explode with "message
+    # type 0x32 arrived from server while idle", which is a mesage
+    # from Postgres.  It's unclear why Pg cares that we closed STDOUT,
+    # but this yak is too big to shave right now.
+    #close STDOUT unless Jifty->web->request->is_subrequest;
 
     # Cleanup
     $self->_handle_stage('CLEANUP');
