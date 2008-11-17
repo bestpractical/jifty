3f74b70a743a2a18eb87cf0a935559ce561b5ba2
Shawn Moore <sartak@bestpractical.com>
2008-11-17 22:56:35 +0000

    Begin working on Google Vizualization "charts", specifically the annotated timeline
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6005 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

---
+++ lib/Jifty/Plugin/Chart/Renderer/GoogleViz.pm
@@ -0,0 +1,8 @@
+package Jifty::Plugin::Chart::Renderer::GoogleViz;
+use strict;
+use warnings;
+use base 'Jifty::Plugin::Chart::Renderer';
+
+
+1;
+
---
+++ lib/Jifty/Plugin/Chart/Renderer/GoogleViz/AnnotatedTimeline.pm
@@ -0,0 +1,8 @@
+package Jifty::Plugin::Chart::Renderer::GoogleViz::AnnotatedTimeline;
+use strict;
+use warnings;
+use base 'Jifty::Plugin::Chart::Renderer::GoogleViz';
+
+
+1;
+
