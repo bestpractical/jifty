bb628590514b64ad4d177092fed0937e34d03458
Shawn Moore <sartak@bestpractical.com>
2008-11-17 23:05:40 +0000

    Superclass init and render
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6006 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/Chart/Renderer/GoogleViz.pm
+++ lib/Jifty/Plugin/Chart/Renderer/GoogleViz.pm
@@ -3,6 +3,31 @@ use strict;
 use warnings;
 use base 'Jifty::Plugin::Chart::Renderer';
 
+=head2 init
+
+We need to load Google's JS.
+
+=cut
+
+sub init {
+    my $self = shift;
+
+    Jifty->web->add_external_javascript("http://www.google.com/jsapi");
+}
+
+=head2 render
+
+=cut
+
+sub render {
+    my $self = shift;
+    if (ref($self) eq __PACKAGE) {
+        Carp::croak("You must use a subclass of GoogleViz, such as GoogleViz::AnnotatedTimeLine");
+    }
+    else {
+        Carp::croak(ref($self) . " does not implement render.");
+    }
+}
 
 1;
 
