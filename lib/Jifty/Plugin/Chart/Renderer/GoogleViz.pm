49000e29ad2eb75114d671abfec50f39d9c1c214
Shawn Moore <sartak@bestpractical.com>
2008-11-18 00:19:00 +0000

    skeleton of GoogleViz->render
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6008 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/Chart/Renderer/GoogleViz.pm
+++ lib/Jifty/Plugin/Chart/Renderer/GoogleViz.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use base 'Jifty::Plugin::Chart::Renderer';
 
+use Jifty::JSON;
+
 =head2 init
 
 We need to load Google's JS.
@@ -21,12 +23,32 @@ sub init {
 
 sub render {
     my $self = shift;
-    if (ref($self) eq __PACKAGE) {
-        Carp::croak("You must use a subclass of GoogleViz, such as GoogleViz::AnnotatedTimeLine");
-    }
-    else {
-        Carp::croak(ref($self) . " does not implement render.");
-    }
+    my $chart_id = 'chart_' . Jifty->web->serial;
+    my $chart_class = $self->chart_class;
+    my $load_params = objToJson {
+        packages => [ $self->packages_to_load ],
+    };
+    my $draw_params = objToJson($self->draw_params);
+
+    Jifty->web->out(<< "JS_HEADER");
+        <script type="text/javascript">
+            google.load('visualization', 1, $load_params);
+            google.setOnLoadCallback(function () {
+                var data = new google.visualization.DataTable();
+JS_HEADER
+
+    $self->render_data;
+
+    Jifty->web->out(<< "JS_FOOTER");
+                var chart = new $chart_class(document.getElementById('$chart_id'));
+                chart.draw(data, $draw_params);
+            });
+        </script>
+JS_FOOTER
+
+    Jifty->web->out(qq{<div id="$chart_id"></div>});
+
+    return;
 }
 
 1;
