64545729d2b7c75f2b2e173f94d3b52d9988e016
Shawn Moore <sartak@bestpractical.com>
2008-11-18 20:59:27 +0000

    AnnotatedTimeline parameters
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6020 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/Chart/Renderer/GoogleViz/AnnotatedTimeline.pm
+++ lib/Jifty/Plugin/Chart/Renderer/GoogleViz/AnnotatedTimeline.pm
@@ -3,6 +3,11 @@ use strict;
 use warnings;
 use base 'Jifty::Plugin::Chart::Renderer::GoogleViz';
 
+use constant packages_to_load => 'annotatedtimeline';
+use constant chart_class => 'google.visualization.AnnotatedTimeLine';
+use constant draw_params => {
+    displayAnnotations => "true",
+};
 
 1;
 
