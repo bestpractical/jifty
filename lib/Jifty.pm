0eedc59fff9951961067ad662e5137846154611f
Alex Vandiver <alexmv@bestpractical.com>
2009-02-21 01:18:07 +0000

    * Update to a modern (and slightly patched, see rt.cpan #35561) Module::Install  * Version bump
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6391 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty.pm
+++ lib/Jifty.pm
@@ -13,7 +13,7 @@ BEGIN {
     require Time::Local;
 
     # Declare early to make sure Jifty::Record::schema_version works
-    $Jifty::VERSION = '0.81208';
+    $Jifty::VERSION = '0.90220';
 }
 
 =head1 NAME
