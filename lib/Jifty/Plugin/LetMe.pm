18bc422c9d2053867b3e97b81c30ae4fcc539e88
Alex Vandiver <alexmv@bestpractical.com>
2009-02-25 06:04:12 +0000

    POD nits for LetMe
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6438 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/LetMe.pm
+++ lib/Jifty/Plugin/LetMe.pm
@@ -4,18 +4,14 @@ use warnings;
 package Jifty::Plugin::LetMe;
 use base qw/Jifty::Plugin/;
 
-# Your plugin goes here.  If takes any configuration or arguments, you
-# probably want to override L<Jifty::Plugin/init>.
-
 =head1 NAME
 
 Jifty::Plugin::LetMe - Plugin for url-based authentication
 
 =head1 DESCRIPTION
 
-This plugin provides dispatcher rules to support jifty's internal C<LetMe> 
-url-based authentication.  
-
+This plugin provides dispatcher rules to support jifty's internal
+C<LetMe> url-based authentication.
 
 =cut
 
