624548819e09aee69ba4569c74c3de3015ec7603
Alex Vandiver <alexmv@bestpractical.com>
2009-02-25 05:24:47 +0000

    Cleanup and dispatcher-ize OnlineDocs
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6413 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/OnlineDocs.pm
+++ lib/Jifty/Plugin/OnlineDocs.pm
@@ -4,16 +4,14 @@ use warnings;
 package Jifty::Plugin::OnlineDocs;
 use base qw/Jifty::Plugin/;
 
-# Your plugin goes here.  If takes any configuration or arguments, you
-# probably want to override L<Jifty::Plugin/init>.
-
 =head1 NAME
 
 Jifty::Plugin::OnlineDocs - Online documentation for Jifty in current application
 
 =head1 DESCRIPTION
 
-This plugin provides superusers with online documentation for Jifty and your application's API. It's included by default when using Jifty. (That's a bug).
+This plugin provides superusers with online documentation for Jifty
+and your application's API, if AdminMode is enabled.
 
 =cut
 
--- lib/Jifty/Plugin/OnlineDocs/Dispatcher.pm
+++ lib/Jifty/Plugin/OnlineDocs/Dispatcher.pm
@@ -13,23 +13,25 @@ Load up the OnlineDocs tab when we're in Admin mode
 
 =cut
 
-
 use Jifty::Dispatcher -base;
     
-
 on '*' => run {
-    if ( Jifty->config->framework('AdminMode') ) {
-        my $top = Jifty->web->navigation;
-        $top->child(
-            OnlineDocs =>
-              url      => "/__jifty/online_docs/",
-            label      => _('Online docs'),
-            sort_order => 999
-        );
-    }
-    return ();
+    return unless Jifty->config->framework('AdminMode');
+
+    my $top = Jifty->web->navigation;
+    $top->child(
+        "OnlineDocs",
+        url        => "/__jifty/online_docs/",
+        label      => _('Online docs'),
+        sort_order => 999
+    );
 };
 
+under '/__jifty/online_docs' => run {
+    return unless Jifty->config->framework('AdminMode');
+    warn "Attempt to access /__jifty/online_docs; either enable AdminMode, or remove the OnlineDocs plugin";
+    redirect('/__jifty/error/permission_denied');
+};
 
 1;
 
