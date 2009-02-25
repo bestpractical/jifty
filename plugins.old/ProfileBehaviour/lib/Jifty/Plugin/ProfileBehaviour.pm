a28daf984021b0604d2caacc85b3dd868641bcb9
Alex Vandiver <alexmv@bestpractical.com>
2009-02-25 04:49:19 +0000

    Remove empty directories and document ProfileBehavior.  It may even work with current Jifty now.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6412 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- plugins.old/ProfileBehaviour/lib/Jifty/Plugin/ProfileBehaviour.pm
+++ plugins.old/ProfileBehaviour/lib/Jifty/Plugin/ProfileBehaviour.pm
@@ -4,7 +4,26 @@ use warnings;
 package Jifty::Plugin::ProfileBehaviour;
 use base qw/Jifty::Plugin/;
 
-# Your plugin goes here.  If takes any configuration or arguments, you
-# probably want to override L<Jifty::Plugin/init>.
+=head1 NAME
+
+Jifty::Plugin::ProfileBehaviour - Overrides behavior.js to add profiling information
+
+=head1 DESCRIPTION
+
+This plugin overrides the stock behavior.js library to add timing and
+profiling information.  Add it if your web pages are slow to style,
+and you want to track down which rules are causing the slowness.
+
+=head1 METHODS
+
+=head2 init
+
+Adds the CSS file needed for on-screen profiling.
+
+=cut
+
+sub init {
+    Jifty->web->add_css('behavior-profile.css');
+}
 
 1;
