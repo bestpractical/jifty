0af55caccb09db69c58d3fedcd6c58c705031570
Alex Vandiver <alexmv@bestpractical.com>
2009-02-25 05:25:10 +0000

    POD cleanup for ActorMetadata
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6417 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/ActorMetadata.pm
+++ lib/Jifty/Plugin/ActorMetadata.pm
@@ -10,22 +10,25 @@ Jifty::Plugin::ActorMetadata - add created_by created_on updated_by updated_on c
 
 =head1 DESCRIPTION
 
-This plugin adds a model mixin which adds C<created_by>, C<created_on>, C<updated_by> and C<updated_on> columns to a model class.
+This plugin adds a model mixin which adds C<created_by>,
+C<created_on>, C<updated_by> and C<updated_on> columns to a model
+class.
 
-=head1 EXAMPLE 
+=head1 SYNOPSIS
 
-use strict;
+ use strict;
  use warnings;
  
- package MeetMeow::Model::Cat;
+ package YourApp::Model::Thingy;
  use Jifty::DBI::Schema;
  
- use MeetMeow::Record schema {
+ use YourApp::Record schema {
  
          ...
  
  };
- use Jifty::Plugin::ActorMetadata::Mixin::Model::ActorMetadata; # created_by, created_on, updated_by and updated_on
+ use Jifty::Plugin::ActorMetadata::Mixin::Model::ActorMetadata;
+ #Provides created_by, created_on, updated_by and updated_on
  
 
 =cut
