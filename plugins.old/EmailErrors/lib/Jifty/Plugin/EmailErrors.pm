b5a1d63ca4a0067a4e95e6d5d570ab76216c39f1
Alex Vandiver <alexmv@bestpractical.com>
2009-02-25 05:24:55 +0000

    Add POD for EmailErrors
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6415 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- plugins.old/EmailErrors/lib/Jifty/Plugin/EmailErrors.pm
+++ plugins.old/EmailErrors/lib/Jifty/Plugin/EmailErrors.pm
@@ -4,8 +4,33 @@ use warnings;
 package Jifty::Plugin::EmailErrors;
 use base qw/Jifty::Plugin/;
 
-# Your plugin goes here.  If takes any configuration or arguments, you
-# probably want to override L<Jifty::Plugin/init>.
+=head1 NAME
+
+Jifty::Plugin::EmailErrors - Emails all 500 pages to an arbitrary email address
+
+=head1 SYNOPSIS
+
+In your config.yml or equivilent:
+
+  Plugins:
+   - EmailErrors:
+       to: address@example.com
+       from: server@example.com
+       subject: Server error
+
+=head1 DESCRIPTION
+
+All errors which result in the browser going to the '500 server error'
+page will send an email with the stack trace that caused it.
+
+=head1 METHODS
+
+=head2 init
+
+Sets up the global values for C<from>, C<to>, and C<subject>, based on
+the plugin's provided configuration.
+
+=cut
 
 sub init {
     my $self = shift;
