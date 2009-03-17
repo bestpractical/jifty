d401b0893397a6244551752aa8fa915fbc0bd50c
Shawn Moore <sartak@bestpractical.com>
2009-03-17 22:40:07 +0000

    We don't need to escape things, json does
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6651 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- share/web/templates/__jifty/config.js
+++ share/web/templates/__jifty/config.js
@@ -1,5 +1,5 @@
-Jifty.Config = <% $js_config %>;
+Jifty.Config = <% $js_config |n%>;
 <%init>
 my $config = Jifty->web->current_user->javascript_config;
-my $js_config = Jifty::JSON::objToJson($config, { singlequote => 1 });
+my $js_config = Jifty::JSON::objToJson($config);
 </%init>
