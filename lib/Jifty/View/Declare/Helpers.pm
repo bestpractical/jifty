4058416fe7a1533eef3aacbebe51d895a5ca86d2
Alex Vandiver <alexmv@bestpractical.com>
2009-04-09 22:54:05 +0000

    Call to _page_class needs parens to not get parsed as a package
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6767 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/View/Declare/Helpers.pm
+++ lib/Jifty/View/Declare/Helpers.pm
@@ -132,7 +132,7 @@ This badly wants to be redone.
 sub wrapper {
     my $content_code = shift;
     my $meta = shift;
-    my $page = _page_class->new({ content_code => $content_code, _meta => $meta });
+    my $page = _page_class()->new({ content_code => $content_code, _meta => $meta });
 
     my ($spa) = Jifty->find_plugin('Jifty::Plugin::SinglePage');
 
