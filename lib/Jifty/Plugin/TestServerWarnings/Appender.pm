66a00dc43a236a97e4203d9a703b450a21870458
Alex Vandiver <alexmv@bestpractical.com>
2009-04-09 22:39:28 +0000

    Add POD for short TestServerWarnings classes
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6756 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/TestServerWarnings/Appender.pm
+++ lib/Jifty/Plugin/TestServerWarnings/Appender.pm
@@ -3,10 +3,34 @@ use strict;
 use warnings;
 use base qw/Log::Log4perl::Appender/;
 
+=head1 NAME
+
+Jifty::Plugin::TestServerWarnings::Appender - Log appender
+
+=head1 DESCRIPTION
+
+L<Log::Log4perl::Appender> which stores warnings in memory, for later
+downloading by the test client.
+
+=head1 METHODS
+
+=head2 new
+
+Creates a new appender; takes no arguments.
+
+=cut
+
 sub new {
     my $class = shift;
     return bless {@_}, $class;
 }
+
+=head2 log
+
+Appends the message to L<Jifty::Plugin::TestServerWarnings>' internal
+storage.
+
+=cut
 
 sub log {
     my $self = shift;
--- lib/Jifty/Plugin/TestServerWarnings/View.pm
+++ lib/Jifty/Plugin/TestServerWarnings/View.pm
@@ -4,6 +4,19 @@ use warnings;
 package Jifty::Plugin::TestServerWarnings::View;
 use Jifty::View::Declare -base;
 
+=head1 NAME
+
+Jifty::Plugin::TestServerWarnings::View - Test warnings downloader
+
+=head1 TEMPLATES
+
+=head2 C<__jifty/test_warnings>
+
+Outputs L<Jifty::Plugin::TestServerWarnings/encoded_warnings> to the
+client.
+
+=cut
+
 template "/__jifty/test_warnings" => sub {
     my $plugin = Jifty->find_plugin('Jifty::Plugin::TestServerWarnings');
     Jifty->handler->apache->content_type("application/x-perl");
