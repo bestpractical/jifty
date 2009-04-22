972481ebf6b0bf512e2cd342cd07084f21f98d6c
Shawn Moore <sartak@bestpractical.com>
2009-04-22 08:37:07 +0000

    RI->get_request(id)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6821 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/RequestInspector.pm
+++ lib/Jifty/Plugin/RequestInspector.pm
@@ -22,6 +22,13 @@ sub init {
 
 sub requests { @requests }
 
+sub get_request {
+    my $self = shift;
+    my $id = shift;
+
+    $requests[$id - 1]; # 1-based
+}
+
 sub new_request_inspection {
     my ($self, $cgi) = @_;
 
