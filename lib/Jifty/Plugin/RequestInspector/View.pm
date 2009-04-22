5814367ebbc02fc16cd172a22a34eb953f5adf70
Shawn Moore <sartak@bestpractical.com>
2009-04-22 07:31:46 +0000

    Begin working on the RI view
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6809 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

---
+++ lib/Jifty/Plugin/RequestInspector/View.pm
@@ -0,0 +1,19 @@
+package Jifty::Plugin::RequestInspector::View;
+use strict;
+use warnings;
+use Jifty::View::Declare -base;
+
+template '/__jifty/admin/requests' => page {
+    my $request_inspector = Jifty->find_plugin('Jifty::Plugin::RequestInspector');
+
+    table {
+        for my $request ($request_inspector->requests) {
+            row {
+                cell { $request->{uri} };
+            };
+        }
+    };
+};
+
+1;
+
