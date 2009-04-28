8bbec134769fddfdb110226c9d7942721d4a0284
sunnavy <sunnavy@bestpractical.com>
2009-04-28 07:50:00 +0000

    add after_restart_url class data for Config plugin
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@6909 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/Config.pm
+++ lib/Jifty/Plugin/Config.pm
@@ -2,11 +2,20 @@ use strict;
 use warnings;
 
 package Jifty::Plugin::Config;
-use base qw/Jifty::Plugin/;
+use base qw/Jifty::Plugin Class::Data::Inheritable/;
+__PACKAGE__->mk_classdata( 'after_restart_url' );
 
 =head2 NAME
 
 Jifty::Plugin::Config - Add configuration editor
+
+=head1 SYNOPSIS
+
+# In your jifty config.yml under the framework section:
+
+  Plugins:
+    - Config:
+        after_restart_url: '/'
 
 =head2  DESCRIPTION
 
@@ -17,9 +26,21 @@ file, so you don't need to edit the config file directly.
 the updated config file will be saved in file $EVN{JIFTY_SITE_CONFIG} or
 etc/site_config.yml
 
-This plugin is designed mostly for beginners ;)
+=head1 METHODS
+
+=head2 init
+
+set after_restart_url, default is '/'
 
 =cut
+
+sub init {
+    my $self = shift;
+    my %opt = @_;
+    if ( $opt{after_restart_url} ) {
+        __PACKAGE__->after_restart_url( $opt{after_restart_url} || '/' );
+    }
+}
 
 1;
 
