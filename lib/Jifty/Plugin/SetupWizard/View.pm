613ae6a9b0179b374352c77dab0afea3f0a5c621
Shawn Moore <sartak@bestpractical.com>
2009-06-01 21:50:48 +0000

    First stab at an extensible setup wizard
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7134 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/SetupWizard/View.pm
+++ lib/Jifty/Plugin/SetupWizard/View.pm
@@ -3,8 +3,63 @@ use strict;
 use warnings;
 use Jifty::View::Declare -base;
 
-template '/__jifty/admin/setupwizard/entry' => page {
-    h1 { "Welcome to " . Jifty->config->framework('ApplicationName') . "!" };
+my @steps = (
+    'language',
+    'database',
+);
+
+template '/__jifty/admin/setupwizard' => page {
+    my $appname = Jifty->config->framework('ApplicationName');
+    h1 { "Welcome to $appname!" };
+
+    render_region(
+        name      => 'WizardStep',
+        path      => "/__jifty/admin/setupwizard/step",
+        arguments => {
+            step => 0,
+        },
+    );
+
+    p { _("You're seeing this configuration because you started $appname in AdminMode and the SetupWizard plugin. Disable one or both of these to restore normal operation.") };
+};
+
+template '/__jifty/admin/setupwizard/step' => sub {
+    my $step = get('step');
+    my $name = $steps[$step] or abort(400);
+
+    show "/__jifty/admin/setupwizard/$name";
+
+    if ($step > 0) {
+        hyperlink(
+            label => _("Back: %1", $steps[$step - 1]),
+            onclick => {
+                replace_self => 1,
+                arguments => {
+                    step => $step - 1,
+                },
+            },
+        );
+    }
+
+    if ($step < @steps - 1) {
+        hyperlink(
+            label => _("Skip: %1", $steps[$step + 1]),
+            onclick => {
+                replace_self => 1,
+                arguments => {
+                    step => $step + 1,
+                },
+            },
+        );
+    }
+};
+
+template '/__jifty/admin/setupwizard/language' => sub {
+    p { _("You may select a different language.") };
+};
+
+template '/__jifty/admin/setupwizard/database' => sub {
+    p { _("You may choose a database engine.") };
 };
 
 1;
