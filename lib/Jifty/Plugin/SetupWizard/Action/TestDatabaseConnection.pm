6f923ab9d96c9b8257ade7993d9b7ee3484ba66d
Shawn Moore <sartak@bestpractical.com>
2009-06-10 23:24:57 +0000

    First take of a TestDatabaseConnection action
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7219 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

---
+++ lib/Jifty/Plugin/SetupWizard/Action/TestDatabaseConnection.pm
@@ -0,0 +1,64 @@
+package Jifty::Plugin::SetupWizard::Action::TestDatabaseConnection;
+use strict;
+use warnings;
+use base 'Jifty::Action';
+
+use Jifty::Param::Schema;
+use Jifty::Action schema {
+    param driver =>
+        is mandatory,
+        type is 'text',
+        default is defer { Jifty->config->framework('Database')->{Driver} };
+
+    param database =>
+        is mandatory,
+        type is 'text',
+        default is defer { Jifty->config->framework('Database')->{Database} };
+
+    param host =>
+        type is 'text',
+        default is defer { Jifty->config->framework('Database')->{Host} };
+
+    param port =>
+        type is 'integer',
+        default is defer { Jifty->config->framework('Database')->{Port} };
+
+    param user =>
+        type is 'text',
+        default is defer { Jifty->config->framework('Database')->{User} };
+
+    param password =>
+        type is 'password',
+        default is defer { Jifty->config->framework('Database')->{Password} };
+
+    param requiressl =>
+        type is 'boolean',
+        default is defer { Jifty->config->framework('Database')->{RequireSSL} };
+};
+
+sub take_action {
+    my $self = shift;
+
+    my $ok = Jifty::DBI::Handle->connect(
+        %{ $self->argument_values },
+    );
+
+    return $ok;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Jifty::Plugin::SetupWizard::Action::TestDatabaseConnection
+
+=head1 METHODS
+
+=head2 take_action
+
+Tests the database connection!
+
+=cut
+
