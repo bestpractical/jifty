5c407e66de29ad8fa45db3e863d284b5fe8c1a39
Alex Vandiver <alexmv@bestpractical.com>
2009-06-30 17:36:31 +0000

    Version bump, changelog updates, and dist prep
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7283 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty.pm
+++ lib/Jifty.pm
@@ -13,7 +13,7 @@ BEGIN {
     require Time::Local;
 
     # Declare early to make sure Jifty::Record::schema_version works
-    $Jifty::VERSION = '0.90519';
+    $Jifty::VERSION = '0.90630';
 }
 
 =head1 NAME
