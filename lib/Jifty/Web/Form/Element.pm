73c42ae62d66100a8787d3d0528f39f97fe8554e
Shawn Moore <sartak@bestpractical.com>
2009-08-14 01:37:27 +0000

    Let users define their cache key with preload's value
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7385 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Web/Form/Element.pm
+++ lib/Jifty/Web/Form/Element.pm
@@ -495,7 +495,11 @@ sub _javascript_attrs_structure {
             $args{toggle} = 1 if $hook->{toggle};
 
             # Preloading functionality
-            $args{preload} = 1 if $hook->{preload};
+            if ($hook->{preload}) {
+                $args{preload} = $hook->{preload} eq "1"
+                               ? Jifty->web->serial
+                               : $hook->{preload};
+            }
 
             # Effects
             $args{$_} = $hook->{$_} for grep {exists $hook->{$_}} qw/effect effect_args remove_effect remove_effect_args/;
