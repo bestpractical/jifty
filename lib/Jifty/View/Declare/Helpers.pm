1d3dc7199f8400fd8ad32dcb83486c612ec34514
Shawn Moore <sartak@bestpractical.com>
2009-09-02 11:57:16 +0000

    Clarify the signature of render_param
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7469 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/View/Declare/Helpers.pm
+++ lib/Jifty/View/Declare/Helpers.pm
@@ -247,9 +247,10 @@ sub render_action(@) {
     }
 }
 
-=head2 render_param $action @args
+=head2 render_param $action, $param, %args
 
-Takes an action and one or more arguments to pass to L<< Jifty::Action->form_field >>.
+Takes an action and one or more arguments to pass to
+L<< Jifty::Action->form_field >>.
 
 =cut
 
