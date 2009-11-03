3d648055ceb7835fe788eed15534865dfbefab89
Jesse Vincent <jesse@bestpractical.com>
2009-11-03 22:50:46 +0000

    pod cleanups
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7584 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Web/Menu.pm
+++ lib/Jifty/Web/Menu.pm
@@ -21,8 +21,8 @@ Jifty::Web::Menu - Handle the API for menu navigation
 =head2 new PARAMHASH
 
 Creates a new L<Jifty::Web::Menu> object.  Possible keys in the
-I<PARAMHASH> are C<label>, C<parent>, C<sort_order>, C<url>, and
-C<active>.  See the subroutines with the respective name below for
+I<PARAMHASH> are L</label>, L</parent>, L</sort_order>, L</url>, and
+L</active>.  See the subroutines with the respective name below for
 each option's use.
 
 =cut
@@ -76,7 +76,7 @@ the parent.  This defaults to adding onto the end.
 
 =head2 link
 
-Gets or set a Jifty::Web::Form::Link object that represents this
+Gets or set a L<Jifty::Web::Form::Link> object that represents this
 menu item. If you're looking to do complex ajaxy things with menus,
 this is likely the option you want.
 
@@ -89,7 +89,7 @@ Get or set the frame or pseudo-target for this link. something like L<_blank>
 =head2 class [STRING]
 
 Gets or sets the CSS class the link should have in addition to the default
-classes.  This is only used if C<link> isn't specified.
+classes.  This is only used if L</link> isn't specified.
 
 =head2 render_children_inline [BOOLEAN]
 
@@ -142,7 +142,7 @@ If only a I<KEY> is provided, returns the child with that I<KEY>.
 
 Otherwise, creates or overwrites the child with that key, passing the
 I<PARAMHASH> to L<Jifty::Web::Menu/new>.  Additionally, the paramhash's
-C<label> defaults to the I<KEY>, and the C<sort_order> defaults to the
+L</label> defaults to the I<KEY>, and the L</sort_order> defaults to the
 pre-existing child's sort order (if a C<KEY> is being over-written) or
 the end of the list, if it is a new C<KEY>.
 
@@ -471,10 +471,10 @@ sub _render_as_yui_menu_item {
 
 =head2 as_link
 
-Return this menu item as a C<Jifty::Web::Form::Link>, either the one
-we were initialized with or a new one made from the C</label> and C</url>
+Return this menu item as a L<Jifty::Web::Form::Link>, either the one
+we were initialized with or a new one made from the L</label> and L</url>
 
-If there's no C</url> and no C</link>, renders just the label.
+If there's no L</url> and no L</link>, renders just the label.
 
 =cut
 
