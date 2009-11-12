239c343b8c967d943369824167cc21497975e13a
Chia-liang Kao <clkao@bestpractical.com>
2009-11-12 11:14:11 +0000

    find uncovered mason files.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7598 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- utils/run_coverage.sh
+++ utils/run_coverage.sh
@@ -3,3 +3,7 @@ rm -rf cover_db
 rm -rf var/mason-cover
 make test HARNESS_PERL_SWITCHES='-MDevel::Cover' $@
 cover -ignore_re '^var/mason-cover/' -ignore_re '^t/'
+egrep -o '(share/[^<]*)' cover_db/coverage.html > cover_db/share-list.txt
+find share/html -type f | xargs -n 1 -I% sh -c 'grep -qx % cover_db/share-list.txt || echo %' > cover_db/uncovered-mason.txt
+echo "Check cover_db/uncovered-mason.txt for uncovered mason files"
+
