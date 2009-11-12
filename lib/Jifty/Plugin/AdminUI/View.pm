498e267e7b746c02917b23b8f0521d6d85d6e73c
Shawn Moore <sartak@bestpractical.com>
2009-11-12 14:05:56 +0000

    Satisfy pod coverage
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7607 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/AdminUI/View.pm
+++ lib/Jifty/Plugin/AdminUI/View.pm
@@ -4,7 +4,6 @@ use warnings;
 use Jifty::View::Declare -base;
 require Jifty::View::Declare::CRUD;
 
-# Mount a crud view for each class
 for my $class (Jifty->class_loader->models) {
     (my $alias = $class) =~ s/.*:://;
     alias Jifty::View::Declare::CRUD under "/__jifty/admin/model/$alias", {
@@ -13,4 +12,14 @@ for my $class (Jifty->class_loader->models) {
 }
 
 1;
+
+=head1 NAME
+
+Jifty::Plugin::AdminUI::View
+
+=head1 DESCRIPTION
+
+Mount a crud view for each class.
+
+=cut
 
