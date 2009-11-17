8b01fb3287a0c92ffeb4751b00e180a3d9497d04
Chia-liang Kao <clkao@bestpractical.com>
2009-11-17 09:43:23 +0000

    find unseen templates from the uncovered list.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/jifty/trunk@7619 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- utils/run_coverage.sh
+++ utils/run_coverage.sh
@@ -7,3 +7,6 @@ egrep -o '(share/[^<]*)' cover_db/coverage.html > cover_db/share-list.txt
 find share/html -type f | xargs -n 1 -I% sh -c 'grep -qx % cover_db/share-list.txt || echo %' > cover_db/uncovered-mason.txt
 echo "Check cover_db/uncovered-mason.txt for uncovered mason files"
 
+sed -e 's/share\/html//' cover_db/uncovered-mason.txt | xargs -n1 -I% sh -c 'grep -qrw % share/html lib/RT/View* || echo %' > cover_db/uncovered-unseen-mason.txt
+
+echo "Check cover_db/uncovered-unseen-mason.txt for uncovered and unseen mason files"
