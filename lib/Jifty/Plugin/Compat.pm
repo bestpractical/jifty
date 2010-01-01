7d8b173b8ff18d66a466f98884847acab9f88352
Chia-liang Kao <clkao@clkao.org>
2010-01-01 12:27:46 +0800

    compat plugin to provide old handler->apache methods

---
+++ lib/Jifty/Plugin/Compat.pm
@@ -0,0 +1,52 @@
+use strict;
+use warnings;
+
+package Jifty::Plugin::Compat;
+use base 'Jifty::Plugin';
+use Hook::LexWrap;
+require Jifty::View::Mason::Handler;
+
+*Jifty::Handler::apache = sub {
+    return 'Jifty::Plugin::Compat::Apache';
+};
+
+wrap 'Jifty::View::Mason::Handler::new',
+    post => sub { my $self = shift;
+                  $self->interp->compiler->add_allowed_globals('$r');
+                  $self->interp->set_global('$r', 'Jifty::Plugin::Compat::Apache');
+              };
+
+package Jifty::Plugin::Compat::Apache;
+use Carp;
+
+sub header_out {
+    my ($class, $key, $val) = @_;
+    if ($key eq 'Status') {
+        Jifty->web->response->status( $val );
+        return;
+    }
+
+    if ($key eq 'Content-Type') {
+        Jifty->web->response->content_type( $val );
+        return;
+    }
+
+    if ($key eq 'Location') {
+        Jifty->web->response->status( 302 );
+    }
+
+    Jifty->web->response->header( $key, $val );
+}
+
+sub content_type {
+    my $class = shift;
+    Jifty->web->response->content_type( @_ );
+}
+
+sub AUTOLOAD {
+    our $AUTOLOAD;
+    croak "compat apache does not handle $AUTOLOAD";
+}
+
+1;
+
