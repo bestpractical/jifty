43eb2d11d8baa59ae8f8e728ffc561b824c7355a
sunnavy <sunnavy@bestpractical.com>
2010-01-27 21:30:17 +0800

    tweak ordered-list.js: make sure head is really head

--- share/web/static/js/ordered-list.js
+++ share/web/static/js/ordered-list.js
@@ -4,7 +4,12 @@ jQuery(document).ready(function(){
         var box = jQuery(this);
         box.find('ul.unselected').sortable( {
             connectWith: box.find('ul.selected'),
-            cancel: 'li.head'
+            cancel: 'li.head',
+            update: function () {
+                if ( jQuery(this).find('li.head').length ) {
+                    jQuery(this).find('li.head').prependTo(this);
+                }
+            }
             });
         box.find('ul.selected').sortable( {
             connectWith: box.find('ul.unselected'),
@@ -12,6 +17,10 @@ jQuery(document).ready(function(){
             update: function () {
                 var select = box.find('select.submit');
                 select.children('option').remove();
+                // make sure head is always the first
+                if ( jQuery(this).find('li.head').length ) {
+                    jQuery(this).find('li.head').prependTo(this);
+                }
                 jQuery(this).find('li:not(.head)').each( function() {
                     var value = jQuery(this).find('input.value').val();
                     jQuery('<option selected="selected" value="' + value + '" >' + value + '</option>').appendTo(select);
