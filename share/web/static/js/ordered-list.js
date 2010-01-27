ef54dcdeab2c6184a6e97fb60397bedd58eabf5b
sunnavy <sunnavy@bestpractical.com>
2010-01-27 20:28:18 +0800

    tweak ordered-list.js

--- share/web/static/js/ordered-list.js
+++ share/web/static/js/ordered-list.js
@@ -1,20 +1,23 @@
 jQuery(document).ready(function(){
-    jQuery("div.ordered-list-container ul.unselected").sortable({
-        connectWith: 'ul.selected',
-        cancel: 'li.head'
+    jQuery("div.ordered-list-container").each(
+        function () {
+        var box = jQuery(this);
+        box.find('ul.unselected').sortable( {
+            connectWith: box.find('ul.selected'),
+            cancel: 'li.head'
+            });
+        box.find('ul.selected').sortable( {
+            connectWith: box.find('ul.unselected'),
+            cancel: 'li.head',
+            update: function () {
+                var select = box.find('select.submit');
+                select.children('option').remove();
+                jQuery(this).find('li:not(.head)').each( function() {
+                    var value = jQuery(this).find('input.value').val();
+                    jQuery('<option selected="selected" value="' + value + '" >' + value + '</option>').appendTo(select);
+    });
+            }
+            });
     });
 
-    jQuery("div.ordered-list-container ul.selected").sortable({
-        connectWith: 'ul.unselected',
-        cancel: 'li.head',
-        update: function () {
-            var box = jQuery(this).parents('div.ordered-list-container');
-            var select = box.find('select.submit');
-            select.children('option').remove();
-            jQuery(this).find('li:not(.head)').each( function() {
-                var value = jQuery(this).find('input.value').val();
-                jQuery('<option selected="selected" value="' + value + '" >' + value + '</option>').appendTo(select);
-    });
-        }
-    });
 });
