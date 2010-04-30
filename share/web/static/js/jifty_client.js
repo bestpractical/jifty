ac745a0dec1500cb9c42e60cfb3332c39ab693e4
Shawn M Moore <sartak@bestpractical.com>
2010-04-30 18:46:37 +0800

    Run the failure handler even if AJAX succeeds

--- share/web/static/js/jifty_client.js
+++ share/web/static/js/jifty_client.js
@@ -23,6 +23,17 @@ Class("JiftyClient", {
             );
         },
         runAction: function (name, params, onSuccess, onFailure) {
+            // if the action returns failure then we want to run the onFailure
+            // handler, even though the AJAX request was successful
+            var onAjaxSuccess = function (result) {
+                if (result.success) {
+                    onSuccess(result);
+                }
+                else {
+                    onFailure(result);
+                }
+            };
+
             jQuery.ajax({
                 url: "/=/action/" + name + ".json",
                 async: false,
@@ -30,7 +41,7 @@ Class("JiftyClient", {
                 dataType: "json",
                 type: 'POST',
                 error: onFailure,
-                success: onSuccess
+                success: onAjaxSuccess
             });
         }
     }
