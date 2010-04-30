bestpractical/jifty

commit 769c800f02ae49a850c4171f6a5fe69af5ce0b55

Rework the ->url logic broken in dae6c2f

lib/Jifty/Web.pm
@@ -120,35 +120,30 @@ form the path part of the resulting URL.
 sub url {
     my $self = shift;
     my %args = (scheme => undef,
-                path => '/',
+                path => undef,
                 @_);
 
     my $uri;
 
-    $uri = Jifty->web->request->uri->clone if Jifty->web->request;
+    my $req = Jifty->web->request;
+    if ($req && $req->uri->host) {
+        $uri = $req->uri->clone;
+        $uri->path('/');
+    }
+    else {
+        $uri = URI->new(Jifty->config->framework("Web")->{BaseURL});
+        $uri->port(Jifty->config->framework("Web")->{Port});
+    }
 
-    if (!$uri) {
-      my $url  = Jifty->config->framework("Web")->{BaseURL};
-      my $port = Jifty->config->framework("Web")->{Port};
-   
-      $uri = URI->new($url);
-      $uri->port($port);
+    if (defined (my $path = $args{path})) {
+        # strip off leading '/' because ->canonical provides one
+        $path =~ s{^/}{};
+        $uri->path_query($path);
     }
 
     # https is sticky
     $uri->scheme('https') if $uri->scheme eq 'http' && Jifty->web->is_ssl;
 
-    if ( defined $args{'scheme'} ) {
-        $uri->scheme( $args{'scheme'} );
-    }
-
-    if (defined $args{path}) {
-      my $path = $args{path};
-      # strip off leading '/' because ->canonical provides one
-      $path =~ s{^/}{};
-      $uri->path_query($path);
-    }
-    
     return $uri->canonical->as_string;
 }
 

commit d9e13544847ea97364a17cfd3320f1527806820d

Fix setting scheme when calling Jifty->web->url
769c800f broke it, but now we have a basic test

lib/Jifty/Web.pm
@@ -150,6 +150,8 @@ sub url {
     # https is sticky
     $uri->scheme('https') if $uri->scheme eq 'http' && Jifty->web->is_ssl;
 
+    $uri->scheme( $args{'scheme'} ) if defined $args{'scheme'};
+
     return $uri->canonical->as_string;
 }
 

t/09-url.t
@@ -10,11 +10,12 @@ Tests that URLs constructed with Jifty->web->url are correct
 
 =cut
 
-use Jifty::Test tests => 5;
+use Jifty::Test tests => 6;
 
 like(Jifty->web->url, qr{^http://localhost:\d+/$}, 'basic call works');
 like(Jifty->web->url(path => 'foo/bar'), qr{^http://localhost:\d+/foo/bar$}, 'path works');
 like(Jifty->web->url(path => '/foo/bar'), qr{^http://localhost:\d+/foo/bar$}, 'path with leading slash works');
+like(Jifty->web->url( scheme => 'https' ), qr{^https://localhost:\d+/$}, 'setting https scheme works');
 
 Jifty::Handler->add_trigger(
     have_request => sub {