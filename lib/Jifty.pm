ff611393f6c8d5f5105a9ce6f061fb4930da5ef3
Shawn M Moore <sartak@bestpractical.com>
2011-02-14 16:59:35 -0500

    Relenging 1.10214

--- lib/Jifty.pm
+++ lib/Jifty.pm
@@ -13,7 +13,7 @@ BEGIN {
     require Time::Local;
 
     # Declare early to make sure Jifty::Record::schema_version works
-    $Jifty::VERSION = '1.01209';
+    $Jifty::VERSION = '1.10214';
 }
 
 =head1 NAME
