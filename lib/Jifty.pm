3d5df8016ae4dc6c7d81b1a8035bafaeeb6241c7
Thomas Sibley <trs@bestpractical.com>
2011-02-28 11:14:36 -0500

    Release engineering for 1.10228

--- lib/Jifty.pm
+++ lib/Jifty.pm
@@ -13,7 +13,7 @@ BEGIN {
     require Time::Local;
 
     # Declare early to make sure Jifty::Record::schema_version works
-    $Jifty::VERSION = '1.10214_01';
+    $Jifty::VERSION = '1.10228';
 }
 
 =head1 NAME
