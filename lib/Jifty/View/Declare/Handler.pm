bestpractical/jifty

commit 070c6eabd593db13921778aff863f39726c475cd

Allow Jifty->web->render_template to take a hashref of args to render with
This allows simpler view-independent rendering of components.

lib/Jifty/View/Declare/Handler.pm
@@ -71,10 +71,17 @@ then print to STDOUT
 sub show {
     my $self     = shift;
     my $template = shift;
+    my $args     = shift;
 
     Template::Declare->buffer( Jifty->handler->buffer );
     eval {
-        Template::Declare::Tags::show_page( $template, { %{Jifty->web->request->arguments}, %{Jifty->web->request->template_arguments || {}} } );
+        Template::Declare::Tags::show_page(
+            $template,
+            $args || {
+                %{Jifty->web->request->arguments},
+                %{Jifty->web->request->template_arguments || {}},
+            },
+        } );
     };
     if (my $err = $@) {
         $err->rethrow if ref $err;

lib/Jifty/Web.pm
@@ -951,7 +951,7 @@ sub template_exists {
 
 my %TEMPLATE_CACHE;
 
-=head2 render_template PATH
+=head2 render_template PATH, [ARGS]
 
 Use our templating system to render a template.  Searches through
 L<Jifty::Handler/view_handlers> to find the first handler which
@@ -966,6 +966,7 @@ C</errors/404> if the template cannot be found.
 sub render_template {
     my $self     = shift;
     my $template = shift;
+    my $args     = shift;
     my $handler;
     my $content;
         my $void_context = ( defined wantarray ? 0 :1);
@@ -1004,7 +1005,7 @@ sub render_template {
     Jifty->handler->buffer->push( private => 1 ) unless $void_context;
 
     Jifty->handler->call_trigger("before_render_template", $handler, $template);
-    eval { $handler->show($template) };
+    eval { $handler->show($template, $args) };
 
     # Handle parse errors
     my $err = $@;

commit 4f3b3c3de3ea54950cb205b638670a63af6baf0f

Fix a syntax error introduced by 070c6eab

lib/Jifty/View/Declare/Handler.pm
@@ -81,7 +81,7 @@ sub show {
                 %{Jifty->web->request->arguments},
                 %{Jifty->web->request->template_arguments || {}},
             },
-        } );
+        );
     };
     if (my $err = $@) {
         $err->rethrow if ref $err;