43d87935ae6ae4edbc239bbb99c335e7a1a06e58
Alex Vandiver <alexmv@bestpractical.com>
2012-05-18 02:50:26 -0400

    Add a simple RPC class atop the message bus

---
+++ lib/Jifty/Plugin/RPC.pm
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+
+package Jifty::Plugin::RPC;
+use base qw/Jifty::Plugin/;
+
+use AnyEvent::RabbitMQ::RPC;
+
+sub prereq_plugins { 'PubSub' }
+
+our $RPC;
+sub init {
+    my $self = shift;
+    my %opt  = @_;
+
+    $RPC = AnyEvent::RabbitMQ::RPC->new(
+        serialize => "Storable",
+        connection => Jifty->bus->_rf,
+    );
+
+    *Jifty::rpc = sub { $RPC };
+}
+
+1;
