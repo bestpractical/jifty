0b2db6abbc7a258f30c260b0edad158392cf1f7c
Alex Vandiver <alexmv@bestpractical.com>
2012-05-18 00:24:50 -0400

    Add a basic Web::Hippie::Pipe endpoint at /__jifty/_hippie
    Using the Jifty->bus AnyMQ object as its base, construct a
    Web::Hippie::Pipe endpoint at /__jifty/_hippie.  This establishes
    bi-directional real-time communication with the client, using wither
    websockets, mxhr (multi-part XMLHttpRequest), or simply polling --
    whichever the browser supports.
    
    This can be used to send messages to the Hippie.Pipe object in the
    browser, by subscribing $listener to topics on new connections (when
    $path is "/new_listener").  Messages recieved from the browser are
    presented as requests to "/message", and disconnects (when the browser
    moves to a new page) are presented as requests to "/error".
    
    For now, simply present a no-op endpoint.

--- lib/Jifty/Plugin/PubSub.pm
+++ lib/Jifty/Plugin/PubSub.pm
@@ -5,10 +5,21 @@ package Jifty::Plugin::PubSub;
 use base qw/Jifty::Plugin/;
 
 use AnyMQ;
+use Plack::Builder;
+use Web::Hippie::App::JSFiles;
 
 sub init {
     my $self = shift;
     my %opt  = @_;
+
+    Jifty->web->add_javascript(
+        qw!
+              pubsub/DUI.js
+              pubsub/Stream.js
+              pubsub/hippie.js
+              pubsub/hippie.pipe.js
+              pubsub/jquery.ev.js
+          ! );
 
     my $anymq = AnyMQ->new_with_traits(
         traits => ['AMQP'],
@@ -21,6 +32,43 @@ sub init {
         %opt,
     );
     *Jifty::bus = sub { $anymq };
+
+    Jifty::View->add_trigger(
+        body_end => sub { $self->body_end }
+    );
+}
+
+sub body_end {
+    my $self = shift;
+    Jifty->web->out( qq|<script type="text/javascript">var hpipe = new Hippie.Pipe(); hpipe.init({path: "/__jifty"})</script>|);
+}
+
+sub psgi_app_static {
+    my $self = shift;
+    my $static_root = $self->static_root;
+    builder {
+        mount '/'          => Plack::App::File->new(root => $static_root)->to_app;
+        mount '/js/pubsub' => Web::Hippie::App::JSFiles->new->to_app;
+    };
+}
+
+sub wrap {
+    my $self = shift;
+    my $app = shift;
+
+    builder {
+        mount '/__jifty/_hippie' => builder {
+            enable "+Web::Hippie";
+            enable "+Web::Hippie::Pipe", bus => Jifty->bus;
+            sub { my $env = shift;
+                  my $listener  = $env->{'hippie.listener'}; # AnyMQ::Queue
+                  my $client_id = $env->{'hippie.client_id'}; # client id
+                  my $path = $env->{PATH_INFO};
+            };
+        };
+
+        mount '/' => $app;
+    };
 }
 
 1;
